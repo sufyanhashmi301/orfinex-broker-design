cf9523ae64f3154253e1ab25a677124d9e738bcc
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2023-12-31 01:57:09 +0500

    dsk

--- app/Console/Commands/MigrateOldData.php
+++ app/Console/Commands/MigrateOldData.php
@@ -64,7 +64,7 @@ class MigrateOldData extends Command
             $balance = $this->getForexAccountBalance($oldForexAccount->login);
 
             if ($balance->isGreaterThan(1)) {
-                echo 'Login : '.$oldForexAccount->login;
+//                echo 'Login : '.$oldForexAccount->login;
                 return true; // Migrate user if any account has balance > 1
             }
         }

0000dbf4214136cd9163b9f2e1e58c81c4934c80
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2023-12-30 19:26:14 +0500

    kj

--- app/Traits/ForexApiTrait.php
+++ app/Traits/ForexApiTrait.php
@@ -1,88 +1,400 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Traits;
 
-use Brick\Math\BigDecimal;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
+use App\Enums\ForexAccountStatus;
 use App\Models\ForexAccount;
+use App\Models\User;
+use Brick\Math\BigDecimal;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Validation\ValidationException;
 
-class MigrateOldData extends Command
+
+trait ForexApiTrait
 {
-    protected $signature = 'migrate:old-data';
-    protected $description = 'Migrate old data from MyISAM tables to new InnoDB tables';
-
-    public function handle()
+    public function getUserApi($login)
     {
-        // Migrate data from users table
-        $usersOldData = DB::connection('old_connection')->table('users')->get();
+        $getUserUrl = config('forextrading.getUserUrl');
 
-        foreach ($usersOldData as $oldUser) {
+        $dataArray = array(
+            'Login' => (int)$login,
 
-
-            // Check if the user has a forex trading account with balance > 1
-            if ($this->hasForexAccountWithBalance($oldUser->id)) {
-                $this->migrateUserToNewDB($oldUser);
+        );
+//        dd($getUserUrl);
+        $response = $this->sendApiRequest($getUserUrl, $dataArray);
+//        dd($response->object(),$response->status());
+        if (isset($response)) {
+            if ($response->status() == 200) {
+                if (isset($response->object()->Login)) {
+                    return $response;
+                }
             }
         }
-
-        $this->info('Data migration completed successfully.');
+        else {
+            throw ValidationException::withMessages([
+                'invalid' => __('Some thing wrong! Please reload the page and try again!')
+            ]);
+        }
     }
 
-    private function hasForexAccountWithBalance($userId)
+    public function sendApiRequest($URL, $dataArray)
     {
-        $forexAccounts = DB::connection('old_connection')
-            ->table('forex_tradings')
-            ->where('user_id', $userId)
-            ->where('account_type', 'real')
-            ->get();
+//        $clientIp = request()->ip();
+////        dd($clientIp);
+//        if(in_array($clientIp,['127.0.0.1' , '::1'])) {
+//            $dataArray['URL'] = $URL;
+//            $localURL = 'https://my.orfinex.com/api/forex';
+////        $localURL = env('EXT_FOREX_URL');
+////            dd($dataArray,$localURL);
+//            $resp = Http::retry(3, 100)->get($localURL, $dataArray);
+//            dd($resp->object());
+////            $data = $request->except(['URL']);
+////            $response = Http::retry(3, 100)->get($request->get('URL'), $data);
+////            dd($response);
+//        }
+//        dd('ss');
+        try {
+            return Http::retry(3, 100)->get($URL, $dataArray);
+        } catch (\GuzzleHttp\Exception\RequestException $exception) {
+            return $exception;
+        }
+    }
+    public function sendApiPostRequest($URL, $dataArray)
+    {
+//        $URL = config('forextrading.depositUrl');
+//
+//        $dataArray = array(
+//            'Login' => 9996864,
+//            'Amount' => 10,
+//            'Comment' => 'testing',
+//        );
+        try {
+            return Http::retry(3, 100)->post($URL, $dataArray);
+        } catch (\GuzzleHttp\Exception\RequestException $exception) {
+            return $exception;
+        }
+    }
 
-        foreach ($forexAccounts as $oldForexAccount) {
-            $balance = $this->getForexAccountBalance($oldForexAccount->login);
+    public function isValidForexAccount($login)
+    {
+        $getUserUrl = config('forextrading.getUserUrl');
 
-            if ($balance->isGreaterThan(1)) {
-                return true; // Migrate user if any account has balance > 1
+        $dataArray = array(
+            'Login' => $login,
+
+        );
+
+        $response = $this->sendApiRequest($getUserUrl, $dataArray);
+        if ($response->status() == 200) {
+            if (isset($response->object()->Login)) {
+                return true;
+            } else {
+                $message = __('The forex account :login is not exist in MT5!.please choose valid account', ['login' => $login]);
+                notify()->error($message, 'Error');
             }
         }
-
-        return false; // No account with balance > 1 found
+        throw ValidationException::withMessages([
+            'invalid' => __('Some thing wrong! Please reload the page and try again!')
+        ]);
     }
-
+//
     public function getForexAccountBalance($login)
     {
-        // Assuming this method returns the balance of the account using the API request
+//        dd($login);
         $getUserResponse = $this->getUserApi($login);
-
         if ($getUserResponse->status() == 200) {
             if (isset($getUserResponse->object()->Login)) {
                 return BigDecimal::of($getUserResponse->object()->Balance);
             } else {
-               return 0;
+                throw ValidationException::withMessages([
+                    'invalid' => __('The forex account :login is not exist in MT5!.please choose valid account', ['login' => $login])
+                ]);
+            }
+        }
+        throw ValidationException::withMessages([
+            'invalid' => __('Some thing wrong! Please reload the page and try again!')
+        ]);
+    }
+
+    public function forexWithdraw($login, $amount, $comment)
+    {
+
+//        $userAccount = ForexTrading::find($transaction->account_from);
+        $withdrawUrl = config('forextrading.withdrawUrl');
+
+        $dataArray = [
+            'Login' => $login,
+            'Amount' => -$amount,
+            'Comment' => $comment,
+
+        ];
+//        dd($userAccount,$dataArray);
+        $withdrawResponse = $this->sendApiPostRequest($withdrawUrl, $dataArray);
+//        dd($withdrawResponse->object());
+        if ($withdrawResponse->status() == 200 && $withdrawResponse->object() == 10009) {
+            return true;
+        } else {
+            $message = __('You do not have enough money! Kindly select valid amount', ['login' => $login]);
+            notify()->error($message, 'Error');
+        }
+    }
+
+    public function forexDeposit($login, $amount, $comment)
+    {
+        $url = config('forextrading.depositUrl');
+
+        $dataArray = [
+            'Login' => $login,
+            'Amount' => $amount,
+            'Comment' => $comment,
+
+        ];
+        $response = $this->sendApiPostRequest($url, $dataArray);
+//        dd($response->object());
+        if ($response->status() == 200 && $response->object() == 10009) {
+            return true;
+        } else {
+            $message = __('The forex account :login is not exist in MT5!.please choose valid account', ['login' => $login]);
+            notify()->error($message, 'Error');
+        }
+    }
+//
+//    public function getTotalHistory($login, $from, $to)
+//    {
+//        $url = config('forextrading.totalHistoryUrl');
+////
+//        $dataArray = array(
+//            'Login' => $login,
+//
+//            'From' => $from,
+//            'To' => $to,
+//        );
+////        dd($dataArray);
+//        return $this->sendApiRequest($url, $dataArray);
+//
+//    }
+//
+//    public function getPageHistory($login, $from, $to)
+//    {
+//        $url = config('forextrading.pageHistoryUrl');
+////
+//        $dataArray = array(
+//            'Login' => $login,
+//
+//            'From' => $from,
+//            'To' => $to,
+//            'Offset' => 0,
+//            'Total' => 100,
+//        );
+////        dd($dataArray);
+//        return $this->sendApiRequest($url, $dataArray);
+//
+//    }
+//
+    public function updateLeverage($login, $leverage)
+    {
+        $url = config('forextrading.updateLeverageUrl');
+
+        $dataArray = array(
+            'Login' => $login,
+            'LeverageAmount' => $leverage,
+        );
+//        dd($dataArray);
+        return $this->sendApiPostRequest($url, $dataArray);
+
+    }
+    public function updateMainPassword($login, $password)
+    {
+        $url = config('forextrading.resetMasterPasswordUrl');
+
+        $dataArray = array(
+            'Login' => $login,
+            'Password' => $password,
+        );
+//        dd($dataArray);
+        return $this->sendApiPostRequest($url, $dataArray);
+
+    }
+    public function updateInvestorPassword($login, $password)
+    {
+        $url = config('forextrading.resetInvestorPasswordUrl');
+
+        $dataArray = array(
+            'Login' => $login,
+            'Password' => $password,
+        );
+//        dd($dataArray);
+        return $this->sendApiPostRequest($url, $dataArray);
+
+    }
+
+    public function disableAccount($login)
+    {
+        $url = config('forextrading.disableAccountUrl');
+
+        $dataArray = array(
+            'Login' => $login,
+
+        );
+//        dd($dataArray);
+        return $this->sendApiRequest($url, $dataArray);
+
+    }
+
+    public function enableAccount($login)
+    {
+        $url = config('forextrading.enableAccountUrl');
+
+        $dataArray = array(
+            'Login' => $login,
+
+        );
+//        dd($dataArray);
+        return $this->sendApiRequest($url, $dataArray);
+
+    }
+//
+//    public function getPageDeal($login, $from, $to)
+//    {
+//        $url = config('forextrading.pageDealUrl');
+////
+//        $dataArray = array(
+//            'Login' => $login,
+//
+//            'From' => $from,
+//            'To' => $to,
+//            'Offset' => 0,
+//            'Total' => 100,
+////            'Comment' => "agent '553862' - #1431717",
+//        );
+////        dd($dataArray);
+//        return $this->sendApiRequest($url, $dataArray);
+//
+//    }
+//
+//
+    public function syncForexAccounts($userID = null)
+    {
+        if (!isset($userID))
+            $userID = auth()->user()->id;
+
+        $realAccounts = ForexAccount::where('user_id', $userID)
+            ->where('status', ForexAccountStatus::Ongoing)
+            ->get();
+
+        $balance = 0;
+        foreach ($realAccounts as $account) {
+//            dd($account);
+            $getUserResponse = $this->getUserApi($account->login);
+//            dd($getUserResponse);
+//           dd($getUserResponse->object(),$getUserResponse->object()->Login);
+            if (isset($getUserResponse)) {
+//                dd($getUserResponse->object(),$getUserResponse->object()->Login);
+
+                if ($getUserResponse->status() == 200 && isset($getUserResponse->object()->Login)) {
+                    $this->updateUserAccount($getUserResponse);
+                    if ($account->account_type == 'real') {
+                        $balance += $getUserResponse->object()->Balance;
+                    }
+
+                }
+
+            }
+        }
+//        dd($balance);
+        $this->updateTotalBalance($userID, $balance);
+
+    }
+
+    public function updateUserAccount($getUserResponse, $lastDeposit = false)
+    {
+        $resData = $getUserResponse->object();
+//        dd($resData);
+//        echo "Login: ".$resData->Login." leverage: ".$resData->Leverage;
+        if (isset($resData->Login)) {
+            $forexTrading = ForexAccount::where('login', $resData->Login)->first();
+//        $forexTrading->account_name = $resData->Name;
+            if($forexTrading) {
+//                $forexTrading->leverage = $resData->Leverage;
+//      $forexTrading->email = $resData->Email;
+                $forexTrading->balance = $resData->Balance;
+                $forexTrading->equity = $resData->Balance;
+                $forexTrading->agent = $resData->Agent;
+//            $forexTrading->free_margin = $resData->MarginFree;
+//            $forexTrading->margin = $resData->Margin;
+                $forexTrading->group = $resData->Group;
+
+                $forexTrading->save();
             }
         }
     }
 
-
-    private function migrateUserToNewDB($olUser)
+    public function updateTotalBalance($userID, $balance)
     {
-        // Migrate user data to the new database
-        $user = User::create([
-            'id' => $olUser->id,
-            // Map other fields accordingly
-        ]);
+        $user = User::where('id', $userID)->first();
+//        $forexTrading->account_name = $resData->Name;
+        $user->balance = $balance;
+        $user->save();
 
-        // Migrate associated forex trading accounts
-        $forexAccounts = DB::connection('old_connection')
-            ->table('forex_tradings')
-            ->where('user_id', $olUser->id)
-            ->get();
-
-        foreach ($forexAccounts as $oldForexAccount) {
-            ForexAccount::on('new_connection')->create([
-                'id' => $oldForexAccount->id,
-                // Map other fields accordingly
-            ]);
-        }
     }
+
+//    public function syncPricingAccount($login)
+//    {
+////            dd($account);
+//        $getUserResponse = $this->getUserApi($login);
+////           dd($getUserResponse->object(),$getUserResponse->object()->Login);
+//        if ($getUserResponse->status() == 200 && isset($getUserResponse->object()->Login)) {
+//            $this->updatePricingInvestmentAccount($getUserResponse);
+//        }
+//
+//
+//    }
+//
+//    public function updatePricingInvestmentAccount($getUserResponse)
+//    {
+//        $resData = $getUserResponse->object();
+////        dd($resData);
+//        if (isset($resData->Login)) {
+//            $pricingInvestment = PricingInvestment::where('login', $resData->Login)->first();
+//            $pricingInvestment->leverage = $resData->Leverage;
+//            $pricingInvestment->current_balance = $resData->Balance;
+//            $pricingInvestment->current_equity = $resData->Equity;
+//
+//            $profit = 0;
+//            if(to_minus($resData->Equity,$pricingInvestment->amount_allotted) > 0){
+//                $profit = to_minus($resData->Equity,$pricingInvestment->amount_allotted);
+//            }
+//            $pricingInvestment->profit = $profit;
+//            $pricingInvestment->group = $resData->Group;
+//            $pricingInvestment->save();
+//        }
+//    }
+//
+//    public function updateLastDeposit($login)
+//    {
+//
+//        $forexTrading = ForexTrading::where('login', $login)->first();
+//        $forexTrading->last_deposit = Carbon::now();
+//        $forexTrading->save();
+////        }
+//    }
+//    public function getPageDealForIBProfits($login, $from, $to,$start,$end)
+//    {
+//        $url = config('forextrading.pageDealUrl');
+////
+//        $dataArray = array(
+//            'Login' => $login,
+//
+//            'From' => $from,
+//            'To' => $to,
+//            'Offset' => $start        ,
+//            'Total' => $end,
+////            'Comment' => "agent '553862' - #1431717",
+//        );
+////        dd($dataArray);
+//        return $this->sendApiRequest($url, $dataArray);
+//
+//    }
+
 }

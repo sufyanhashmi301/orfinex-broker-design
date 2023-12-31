31ee0f5074db50cf848d9d84cd6695611359bf85
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2023-12-31 23:54:10 +0500

    sjkf

--- app/Http/Controllers/Frontend/ReferralController.php
+++ app/Http/Controllers/Frontend/ReferralController.php
@@ -34,14 +34,14 @@ class ReferralController extends Controller
         $level = LevelReferral::max('the_order');
         $balance = BigDecimal::of(0);
 //        dd(auth()->user()->ib_login);
-//        if(auth()->user()->ib_login) {
-//            $getUserResponse = $this->getUserApi(auth()->user()->ib_login);
-////            dd($getUserResponse->object());
-//            if ($getUserResponse->status() == 200 && isset($getUserResponse->object()->Login)) {
-////                $this->updateUserAccount($getUserResponse);
-//                $balance = $getUserResponse->object()->Balance;
-//            }
-//        }
+        if(auth()->user()->ib_login) {
+            $getUserResponse = $this->getUserApi(auth()->user()->ib_login);
+//            dd($getUserResponse->object());
+            if ($getUserResponse->status() == 200 && isset($getUserResponse->object()->Login)) {
+//                $this->updateUserAccount($getUserResponse);
+                $balance = $getUserResponse->object()->Balance;
+            }
+        }
         $ibQuestions = IbQuestion::where('status', true)->get();
         $data =
         $qrCode = QrCode::size(300)->generate($getReferral->link);

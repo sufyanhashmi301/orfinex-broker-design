b88c0e8ff354349a87e5e7b8dd7f54e571c4aade
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2023-12-31 23:14:03 +0500

    sdfj

--- app/Http/Controllers/Frontend/ForexAccountController.php
+++ app/Http/Controllers/Frontend/ForexAccountController.php
@@ -79,7 +79,7 @@ class ForexAccountController extends GatewayController
         $data['Country'] = auth()->user()->country;
         $data['Login'] = 0;
         $data['Language'] = 0;
-        $data['Rights'] = 'USER_RIGHT_ALL';
+        $data['Rights'] = 'USER_RIGHT_ENABLED';
         $data['Status'] = 'YES';
 
         $URL = config('forextrading.createUserUrl');

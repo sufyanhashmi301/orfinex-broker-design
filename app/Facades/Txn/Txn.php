483134b6342ff24084fc952d0923731f2465faac
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-01-01 17:05:31 +0500

    migrtion

--- app/Facades/Txn/Txn.php
+++ app/Facades/Txn/Txn.php
@@ -72,7 +72,7 @@ class Txn
 
         if ($status == 'success' && ($transaction->type == TxnType::Deposit || $transaction->type == TxnType::ManualDeposit)) {
             if (isset($transaction->target_id) && $transaction->target_type == 'forex_deposit') {
-                $comment =  "Deposit/".$transaction->tnx;
+                $comment =  "$transaction->method/".$transaction->tnx;
                 $this->ForexDeposit($transaction->target_id,$transaction->final_amount,$comment);
             } else {
                 $amount = $transaction->amount;

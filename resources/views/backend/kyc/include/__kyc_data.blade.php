d69a440d2069a0d67dc40fbb4f579544fbd67bf4
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-01-05 00:58:06 +0500

    kycs

--- resources/views/backend/kyc/include/__kyc_data.blade.php
+++ resources/views/backend/kyc/include/__kyc_data.blade.php
@@ -6,11 +6,12 @@
 
     @foreach( $kycCredential as $key => $value)
         <li class="list-group-item">
-            {{ $key }}: @if( file_exists('assets/'.$value))
+            {{ $key }}:
+{{--            @if(file_exists('assets/'.$value))--}}
                 <img src="{{ asset($value) }}" alt=""/>
-            @else
-                <strong>{{ $value }}</strong>
-            @endif
+{{--            @else--}}
+{{--                <strong>{{ $value }}</strong>--}}
+{{--            @endif--}}
         </li>
     @endforeach
 </ul>

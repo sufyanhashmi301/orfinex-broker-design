812031de3971332c4a3eb24f786ff8b8604e01e7
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-01-19 17:11:18 +0500

    asdjk

--- resources/views/backend/transaction/include/__user.blade.php
+++ resources/views/backend/transaction/include/__user.blade.php
@@ -1,4 +1,5 @@
 @php
     $user = \App\Models\User::find($user_id);
 @endphp
-<a class="link" href="{{ route('admin.user.edit',$user->id) }}">{{ safe($user->username) }}<br><span>{{$user->email}}</span></a>
+
+<a class="link" href="{{ route('admin.user.edit',$user->id) }}"><span class="d-block lh-1 mb-2">{{ safe($user->username) }}</span><span class="d-block lh-1">{{$user->email}}</span></a>

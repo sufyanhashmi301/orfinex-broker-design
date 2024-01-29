53cf6e6a4e7958b8c7ecce20caae7a32dfd6199f
naeemmunir <naeemali2020@gmail.com>
2024-01-29 16:54:03 +0500

    trade modal design.

--- resources/views/frontend/default/user/forex/dropdown-menu.blade.php
+++ resources/views/frontend/default/user/forex/dropdown-menu.blade.php
@@ -20,7 +20,10 @@
                     {{ __('Withdraw') }}
                 </a>
             @endif
-            <a href="" class="text-center">
+            <a class="text-center" href=""
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#tradeModal">
                 <div class="lg:h-[32px] lg:w-[32px] lg:bg-slate-100 lg:dark:bg-slate-900 dark:text-white text-slate-900 cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center mx-auto">
                     <iconify-icon class="text-slate-800 dark:text-white text-lg" icon="tabler:chart-candle"></iconify-icon>
                 </div>

5366b6cdfc49f027578fff20e684d4cd6f128f4d
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-02-01 22:32:31 +0500

    imports

--- app/Imports/UserImportClass.php
+++ app/Imports/UserImportClass.php
@@ -12,18 +12,31 @@ class UserImportClass implements ToModel
     public function model(array $row)
     {
 //        if(!UserImport::where('email',$row[6])->exists()) {
-            return new UserImport([
-                'login' => $row[0],
-                'f_name' => $row[1],
-                'l_name' => $row[2],
-                'm_name' => $row[3],
-                'group' => $row[4],
-                'country' => $row[5],
-                'email' => $row[6],
-                'leverage' => $row[7],
-                'register_time' => isset($row[15]) ? Carbon::createFromFormat('Y.m.d H:i', $row[15])->format('Y-m-d H:i:s') : Carbon::now()
-                // Map other columns as needed
-            ]);
+//            return new UserImport([
+//                'login' => $row[0],
+//                'f_name' => $row[1],
+//                'l_name' => $row[2],
+//                'm_name' => $row[3],
+//                'group' => $row[4],
+//                'country' => $row[5],
+//                'email' => $row[6],
+//                'leverage' => $row[7],
+//                'register_time' => isset($row[15]) ? Carbon::createFromFormat('Y.m.d H:i', $row[15])->format('Y-m-d H:i:s') : Carbon::now()
+//                // Map other columns as needed
+//            ]);
 //        }
+
+        return new UserImport([
+//            'login' => $row[0],
+//            'f_name' => $row[1],
+//            'l_name' => $row[2],
+//            'm_name' => $row[3],
+//            'group' => $row[4],
+//            'country' => $row[5],
+            'email' => $row[1],
+//            'leverage' => $row[7],
+//            'register_time' => isset($row[15]) ? Carbon::createFromFormat('Y.m.d H:i', $row[15])->format('Y-m-d H:i:s') : Carbon::now()
+            // Map other columns as needed
+        ]);
     }
 }

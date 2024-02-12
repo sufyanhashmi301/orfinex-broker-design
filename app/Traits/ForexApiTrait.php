4e089f0aa767d520e73c217800c7bcfcf10674be
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-02-12 16:11:08 +0500

    getibapi

--- app/Traits/ForexApiTrait.php
+++ app/Traits/ForexApiTrait.php
@@ -38,6 +38,31 @@ trait ForexApiTrait
             return false;
         }
     }
+    public function getUserIBApi($login)
+    {
+        $getUserUrl = config('forextrading.getUserUrl');
+
+        $dataArray = array(
+            'Login' => (int)$login,
+        );
+//        dd($getUserUrl);
+        $response = $this->sendApiRequest($getUserUrl, $dataArray);
+//        dd($login,$response->object(),$response->status());
+        if (isset($response)) {
+            if ($response->status() == 200) {
+                if ($response->object()->Login != 0) {
+                    return $response;
+                }
+                else{
+                    User::where('ib_login',$login)->update(['ib_status'=>ForexAccountStatus::Unknown]);
+                    return false;
+                }
+            }
+        }
+        else {
+            return false;
+        }
+    }
     public function getRoiApi($login)
     {
         $getUserUrl = config('forextrading.getRoiUrl').'?login=<Login>';

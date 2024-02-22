55bcaeca698dc8a3f69a3e51a3df793e6fe2ff32
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-02-22 14:21:33 +0500

    update users

--- app/Console/Commands/UpdateNames.php
+++ app/Console/Commands/UpdateNames.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\User;
+use App\Models\UserImport;
 use Illuminate\Console\Command;
 
 class UpdateNames extends Command
@@ -31,32 +32,41 @@ class UpdateNames extends Command
         $lname = 'lname';
         // Get users with specified last name
         $users = User::all();
-
-        foreach ($users as $user) {
-            if (isset($user->last_name)) {
-                // Split the first name into an array of words
-                $firstNameParts = explode(' ', $user->first_name);
-
-                // Determine the number of parts in the first name
-                $numParts = count($firstNameParts);
-
-                if ($numParts == 1) {
-                    // If only one part, replace last_name with single value
-                    $user->last_name = $firstNameParts[0];
-                } elseif ($numParts == 2) {
-                    // If two parts, replace first_name with the first value and last_name with the second value
-                    $user->first_name = $firstNameParts[0];
-                    $user->last_name = $firstNameParts[1];
-                } elseif ($numParts >= 3) {
-                    // If three or more parts, replace first_name with the first two values and last_name with the third value
-                    $user->first_name = implode(' ', array_slice($firstNameParts, 0, 2));
-                    $user->last_name = $firstNameParts[2];
-                }
-
-                // Save the updated user
+        $users =  UserImport::all();
+        foreach ($users as $userImport) {
+            $user= User::where('email',$userImport->email)->first();
+            if($user){
+                $user->first_name = $userImport->f_name;
+                $user->last_name = $userImport->l_name;
                 $user->save();
             }
+
+
         }
+//            if (isset($user->last_name)) {
+//                // Split the first name into an array of words
+//                $firstNameParts = explode(' ', $user->first_name);
+//
+//                // Determine the number of parts in the first name
+//                $numParts = count($firstNameParts);
+//
+//                if ($numParts == 1) {
+//                    // If only one part, replace last_name with single value
+//                    $user->last_name = $firstNameParts[0];
+//                } elseif ($numParts == 2) {
+//                    // If two parts, replace first_name with the first value and last_name with the second value
+//                    $user->first_name = $firstNameParts[0];
+//                    $user->last_name = $firstNameParts[1];
+//                } elseif ($numParts >= 3) {
+//                    // If three or more parts, replace first_name with the first two values and last_name with the third value
+//                    $user->first_name = implode(' ', array_slice($firstNameParts, 0, 2));
+//                    $user->last_name = $firstNameParts[2];
+//                }
+//
+//                // Save the updated user
+//                $user->save();
+//            }
+//        }
 
 //        return $users;
     }

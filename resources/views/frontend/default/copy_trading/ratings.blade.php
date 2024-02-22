e3f9c54c34d35d606c7adc008ffe739ba19823e4
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-02-22 16:01:32 +0500

    rating

--- resources/views/frontend/default/copy_trading/ratings.blade.php
+++ resources/views/frontend/default/copy_trading/ratings.blade.php
@@ -10,6 +10,6 @@
     </style>
 @endsection
 @section('content')
-    <iframe id="widgetFrame" style="min-width: 100%;" src="https://copytrader.mbfx.co/widgets/ratings?widgetKey=social-ratings&theme=light&lang=en" scrolling="no" frameborder="0" onload="iFrameResize({ heightCalculationMethod: 'max', checkOrigin: false }, '#widgetFrame')"></iframe>
-    <script src="https://copytrader.mbfx.co/widgets/assets/js/iframeResizer.js"></script>
+    <iframe id="widgetFrame" style="min-width: 100%;" src="https://brokeree.mbfx.co/widgets/ratings?widgetKey=social-ratings&theme=light&lang=en" scrolling="no" frameborder="0" onload="iFrameResize({ heightCalculationMethod: 'max', checkOrigin: false }, '#widgetFrame')"></iframe>
+    <script src="https://brokeree.mbfx.co/widgets/assets/js/iframeResizer.js"></script>
 @endsection

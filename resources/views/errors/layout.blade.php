ad9c39e8b49e5cd7cf807f9d7a53b67e059f911b
naeemmunir <naeemali2020@gmail.com>
2024-02-27 13:11:41 +0500

    email template and bonus page design.

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -40,17 +40,31 @@
         }
 
         .back-to-home-btn {
+            --tw-bg-opacity: 1;
+            background-color: rgb(15 23 42 / var(--tw-bg-opacity));
+            --tw-text-opacity: 1;
+            color: rgb(255 255 255 / var(--tw-text-opacity));
+            --tw-ring-opacity: 1;
+            --tw-ring-color: rgb(15 23 42 / var(--tw-ring-opacity));
             display: inline-block;
             padding: 13px 27px;
             border-radius: 50px;
             color: #ffffff;
-            background: #e73667;
+            /* background: #e73667; */
             font-weight: 500;
             font-size: 13px;
             text-transform: uppercase;
             box-shadow: 0px 0px 2px #00304966;
             text-decoration: none;
             margin-top: 20px;
+        }
+
+        .back-to-home-btn:hover {
+            --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+            --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+            box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+            --tw-ring-opacity: 0.8;
+            --tw-ring-offset-width: 1px;
         }
 
         @media (max-width: 991px) {
@@ -84,7 +98,7 @@
     </style>
 </head>
 
-<body class="dark-bg">
+<body class="light-bg">
 
 <div class="unusual-page">
     @yield('content')

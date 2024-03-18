60bd81d674e7bc20ba252d01a1e34a18febc82c7
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-03-18 17:28:45 +0500

    auto kyc samsub colums for users table

---
+++ database/migrations/2024_03_01_115643_add_kyc_columns_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('kyc_token', 500)->nullable()->after('kyc_credential');
+            $table->timestamp('kyc_created_at')->nullable()->after('kyc_token');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('kyc_token');
+            $table->dropColumn('kyc_created_at');
+        });
+    }
+};

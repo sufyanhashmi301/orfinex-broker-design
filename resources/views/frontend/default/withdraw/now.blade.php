755f17a5f06b05d741895f0c9222788468ead74b
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-04-04 15:39:31 +0500

    muliti IB withdrawable

--- resources/views/frontend/default/withdraw/now.blade.php
+++ resources/views/frontend/default/withdraw/now.blade.php
@@ -58,6 +58,11 @@
                                                         class="inline-block font-Inter font-normal text-sm text-slate-600">{{ auth()->user()->ib_login }}
                                                     - {{ __('IB') }} ({{ auth()->user()->ib_balance }} {{$currency}})</option>
                                             @endif
+                                            @if(isset(auth()->user()->multi_ib_login))
+                                                <option value="{{ auth()->user()->multi_ib_login }}" data-type="mib-account"
+                                                        class="inline-block font-Inter font-normal text-sm text-slate-600">{{ auth()->user()->multi_ib_login }}
+                                                    - {{ __('Multi IB') }} ({{ auth()->user()->multi_ib_balance }} {{$currency}})</option>
+                                            @endif
                                         </select>
                                     </div>
                                 </div>

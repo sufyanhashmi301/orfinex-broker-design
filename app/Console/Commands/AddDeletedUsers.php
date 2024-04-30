744e0c4ee7da7642e7d911fd0755337010d6d08e
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-04-30 14:01:06 +0500

    dleeted accounts

--- app/Console/Commands/AddDeletedUsers.php
+++ app/Console/Commands/AddDeletedUsers.php
@@ -3,12 +3,21 @@
 namespace App\Console\Commands;
 
 use App\Models\ForexAccount;
+use App\Models\IbQuestionAnswer;
+use App\Models\LoginActivities;
+use App\Models\Message;
+use App\Models\Notification;
 use App\Models\Ranking;
+use App\Models\ReferralLink;
+use App\Models\ReferralRelationship;
+use App\Models\Ticket;
 use App\Models\Transaction;
 use App\Models\User;
+use App\Models\WithdrawAccount;
 use App\Traits\ForexApiTrait;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -53,94 +62,52 @@ class AddDeletedUsers extends Command
             }
 
             $getUserResponse = $this->getUserInfoUrl($missingUser->login);
-            $data = $getUserResponse->object();
+
 //            $getUserResponse = $this->getUserInfoUrl(874641);
 //                dd($getUserResponse->object());
             if (!$getUserResponse) {
                 echo 'Account Not exist: ' . $missingUser->login . "\n";
                 continue;
             }
-
+            $data = $getUserResponse->object();
             if ($data->Login == 0) {
                 echo 'Account Not exist: ' . $missingUser->login . "\n";
                 continue;
             }
-            if ($missingUser->account_type == 'real' && ($data->Balance > 0 || $data->Equity > 0)) {
-                echo 'Account Not exist: ' . $missingUser->login . ' balance: ' . $data->Balance . "\n";
-            }
 //            dd($data);
-            echo 'Missing Email: ' . $data->Email . "\n";
+//            if (User::where('email', $data->Email)->exists()) {
+//                echo 'Email: ' . $data->Email . ' already exists' . "\n";
+//                continue;
+//            }
 
-            if (User::where('email', $data->Email)->exists()) {
-                echo 'Email: ' . $data->Email . ' already exists' . "\n";
-                continue;
-            }
+            if ($missingUser->account_type == 'real' && $data->Balance > 0) {
+                if (User::where('email', $data->Email)->exists()) {
+                    echo 'Email: ' . $data->Email . ' already exists' . "\n";
+                    $newUser = User::where('email', $data->Email)->first();
+//                    $this->updateRecordWithNewUser($newUser, $missingUserOldID);
 
-            if ($data->Agent != 0) {
-//                $IbParent = $this->getUserInfoUrl(874641);
-                $IbParent = $this->getUserInfoUrl($data->Agent);
-                $IbParent = $IbParent->object();
-                if ($IbParent->Login == 0) {
-                    echo 'IB Account Not exist in Mt5: ' . $data->Agent . "\n";
-//                    continue;
                 } else {
-                    $parentUser = User::where('email', $IbParent->Email)->first();
-
-                    echo 'Parent Email: ' . $parentUser->email . ' of ' . $data->Email . "\n";
+                    echo 'Missing Email: ' . $data->Email .' Account: ' . $missingUser->login . ' balance: ' . $data->Balance . "\n";
+//                    $newUser = $this->createUser($data, $missingUserOldID);
+//                    $newUser =  $this->updateRecordWithNewUser($newUser, $missingUserOldID);
                 }
 
             }
-            $firstNameParts = explode(' ', $data->Name);
-
-            // Determine the number of parts in the first name
-            $numParts = count($firstNameParts);
-
-            if ($numParts == 1) {
-                // If only one part, replace last_name with single value
-                $firstName = $data->Name;
-                $lastName = $firstNameParts[0];
-            } elseif ($numParts == 2) {
-                // If two parts, replace first_name with the first value and last_name with the second value
-                $firstName = $firstNameParts[0];
-                $lastName = $firstNameParts[1];
-            } elseif ($numParts >= 3) {
-                // If three or more parts, replace first_name with the first two values and last_name with the third value
-                $firstName = implode(' ', array_slice($firstNameParts, 0, 2));
-                $lastName = $firstNameParts[2];
-            }
-
-            $rank = Ranking::find(1);
-            $newUser = new User();
-// Set the attributes for the new user
-            $newUser->id = $missingUserOldID; // Assign the deleted ID to the new user
-            $newUser->ranking_id = $rank->id;
-            $newUser->rankings = json_encode([$rank->id]);
-            $newUser->first_name = $firstName;
-            $newUser->last_name = $lastName;
-            $newUser->username = $firstName . $lastName . rand(1000, 9999);
-            $newUser->country = $data->Country;
-            $newUser->phone = $data->Phone;
-            $newUser->email = $data->Email;
-            $newUser->password = Hash::make('orfinex123');
-            $newUser->kyc = 1;
-            $newUser->email_verified_at = Carbon::now();
-
-// Save the new user to the database
-//            $newUser->save();
-
-
-//            $user = User::create([
-//                'id' => $rank->id,
-//                'ranking_id' => $rank->id,
-//                'rankings' => json_encode([$rank->id]),
-//                'first_name' => $firstName,
-//                'last_name' => $lastName,
-//                'username' => $firstName . $lastName . rand(1000, 9999),
-//                'country' => $data->Country,
-//                'phone' => $data->Phone,
-//                'email' => $data->Email,
-//                'password' => Hash::make('orfinex123'),
-//            ]);
+//            echo 'Missing Email: ' . $data->Email . "\n";
+//            if ($data->Agent != 0) {
+////                $IbParent = $this->getUserInfoUrl(874641);
+//                $IbParent = $this->getUserInfoUrl($data->Agent);
+//                $IbParent = $IbParent->object();
+//                if ($IbParent->Login == 0) {
+//                    echo 'IB Account Not exist in Mt5: ' . $data->Agent . "\n";
+////                    continue;
+//                } else {
+//                    $parentUser = User::where('email', $IbParent->Email)->first();
+//
+//                    echo 'Parent Email: ' . $parentUser->email . ' of ' . $data->Email . "\n";
+//                }
+//
+//            }
 
 //            Login::where('user_id',$missingUserOldID)->update(['user_id'=>$user->id]);
 //            ForexAccount::where('user_id',$missingUserOldID)->update(['user_id'=>$user->id]);
@@ -148,6 +115,84 @@ class AddDeletedUsers extends Command
 
         }
 
+
         return Command::SUCCESS;
+    }
+
+    public function createUser($data, $missingUserOldID)
+    {
+        $firstNameParts = explode(' ', $data->Name);
+
+        // Determine the number of parts in the first name
+        $numParts = count($firstNameParts);
+
+        if ($numParts == 1) {
+            // If only one part, replace last_name with single value
+            $firstName = $data->Name;
+            $lastName = $firstNameParts[0];
+        } elseif ($numParts == 2) {
+            // If two parts, replace first_name with the first value and last_name with the second value
+            $firstName = $firstNameParts[0];
+            $lastName = $firstNameParts[1];
+        } elseif ($numParts >= 3) {
+            // If three or more parts, replace first_name with the first two values and last_name with the third value
+            $firstName = implode(' ', array_slice($firstNameParts, 0, 2));
+            $lastName = $firstNameParts[2];
+        }
+
+        $rank = Ranking::find(1);
+        $newUser = new User();
+// Set the attributes for the new user
+        $newUser->id = $missingUserOldID; // Assign the deleted ID to the new user
+        $newUser->ranking_id = $rank->id;
+        $newUser->rankings = json_encode([$rank->id]);
+        $newUser->first_name = $firstName;
+        $newUser->last_name = $lastName;
+        $newUser->username = $firstName . $lastName . rand(1000, 9999);
+        $newUser->country = $data->Country;
+        $newUser->phone = $data->Phone;
+        $newUser->email = $data->Email;
+        $newUser->password = Hash::make('orfinex123');
+        $newUser->kyc = 1;
+        $newUser->email_verified_at = Carbon::now();
+
+        // Save the new user to the database
+        $newUser->save();
+        return $newUser;
+    }
+
+    public function updateRecordWithNewUser($missingUserOldID, $newUser)
+    {
+//            Login::where('user_id',$missingUserOldID)->update(['user_id'=>$newUser->id]);
+        LoginActivities::where('user_id', $missingUserOldID)->delete();
+        Notification::where('user_id', $missingUserOldID)->delete();
+        ReferralLink::where('user_id', $missingUserOldID)->delete();
+        ReferralRelationship::where('user_id', $missingUserOldID)->delete();
+        ReferralRelationship::where('referral_link_id', $missingUserOldID)->delete();
+
+        User::where('ref_id', $missingUserOldID)->update(['ref_id' => null]);
+        Ticket::where('user_id', $missingUserOldID)->update(['user_id' => $newUser->id]);
+        Message::where('user_id', $missingUserOldID)->delete();
+        WithdrawAccount::where('user_id', $missingUserOldID)->update(['user_id' => $newUser->id]);
+        ForexAccount::where('user_id', $missingUserOldID)->update(['user_id' => $newUser->id]);
+        IbQuestionAnswer::where('user_id', $missingUserOldID)->update(['user_id' => $newUser->id]);
+        Transaction::where('user_id', $missingUserOldID)->update(['user_id' => $newUser->id]);
+    }
+    public function deleteRecords($missingUserOldID)
+    {
+//            Login::where('user_id',$missingUserOldID)->update(['user_id'=>$newUser->id]);
+        LoginActivities::where('user_id', $missingUserOldID)->delete();
+        Notification::where('user_id', $missingUserOldID)->delete();
+        ReferralLink::where('user_id', $missingUserOldID)->delete();
+        ReferralRelationship::where('user_id', $missingUserOldID)->delete();
+        ReferralRelationship::where('referral_link_id', $missingUserOldID)->delete();
+
+        User::where('ref_id', $missingUserOldID)->update(['ref_id' => null]);
+        Ticket::where('user_id', $missingUserOldID)->delete();
+        Message::where('user_id', $missingUserOldID)->delete();
+        WithdrawAccount::where('user_id', $missingUserOldID)->delete();
+        ForexAccount::where('user_id', $missingUserOldID)->delete();
+        IbQuestionAnswer::where('user_id', $missingUserOldID)->delete();
+        Transaction::where('user_id', $missingUserOldID)->delete();
     }
 }

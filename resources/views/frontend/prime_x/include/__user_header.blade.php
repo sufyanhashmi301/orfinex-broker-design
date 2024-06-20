903e1ea5918e46fd2d7b3acce146a2cd1a26f0cb
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-06-20 18:31:04 +0500

    header url

--- resources/views/frontend/prime_x/include/__user_header.blade.php
+++ resources/views/frontend/prime_x/include/__user_header.blade.php
@@ -28,7 +28,7 @@
                     <div class="dropdown-menu z-10 hidden bg-white divide-y divide-slate-100 shadow min-w-max dark:bg-slate-800 border dark:border-slate-700 !top-[23px] rounded-md overflow-hidden">
                         <ul class="py-1 text-sm text-slate-800 dark:text-slate-200">
                             <li>
-                                <a href="{{ route('user.setting.show') }}" class="block px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600 dark:hover:text-white font-inter text-sm text-slate-600 dark:text-white font-normal">
+                                <a href="{{ route('user.setting.profile') }}" class="block px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600 dark:hover:text-white font-inter text-sm text-slate-600 dark:text-white font-normal">
                                     <iconify-icon icon="lucide:settings" class="relative top-[2px] text-lg ltr:mr-1 rtl:ml-1"></iconify-icon>
                                     <span class="font-Inter">{{ __('Settings') }}</span>
                                 </a>
@@ -66,7 +66,7 @@
                     </div>
                 </div>
                 <!-- Profile DropDown Area -->
-                
+
                 <!-- BEGIN: Notification Dropdown -->
                 @auth
                     @php

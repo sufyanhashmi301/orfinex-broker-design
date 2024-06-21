6ae18d4ee9626edbfc044e6e5fbd3ca0e0648f26
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-06-21 05:31:10 +0500

    head

--- resources/views/frontend/prime_x/include/__head.blade.php
+++ resources/views/frontend/prime_x/include/__head.blade.php
@@ -4,7 +4,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="keywords" content="@yield('meta_keywords',setting('site_title','global'))">
     <meta name="description" content="@yield('meta_description',setting('site_title','global'))">
-    <meta property="og:image" content="https://orfinex.com/assets/img/orfinex-cover.jpg">
+    <meta property="og:image" content="{{env('APP_URL')}}.'/assets/images/ZTegliep6F7miE8fiA9e.png">
     <link rel="canonical" href="{{ url()->current() }}"/>
     <link rel="shortcut icon" href="{{ asset(setting('site_favicon','global')) }}" type="image/x-icon"/>
     <link rel="icon" href="{{ asset(setting('site_favicon','global')) }}" type="image/x-icon"/>

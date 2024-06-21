fc4655ae7ec468a9eadf5e3f7a321ac487752f52
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-06-21 05:06:16 +0500

    dashboard

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -19,10 +19,10 @@ class DashboardController extends Controller
 //        dd(getLocation(),'dashboar');
         $user = auth()->user();
 
-        $clientIp = request()->ip();
-        if(!in_array($clientIp,['127.0.0.1' , '::1'])) {
-            sync_forex_accounts(auth()->id());
-        }
+//        $clientIp = request()->ip();
+//        if(!in_array($clientIp,['127.0.0.1' , '::1'])) {
+//            sync_forex_accounts(auth()->id());
+//        }
 //        if(!$user->ref_id) {
 //            AgentReferralJob::dispatch($user);
 //        }

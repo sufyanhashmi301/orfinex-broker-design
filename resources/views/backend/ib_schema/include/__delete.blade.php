32a1bccdb52569957b247f2c72624433b41bfa0e
naeemmunir <naeemali2020@gmail.com>
2024-07-22 22:07:03 +0500

    delete modal design

--- resources/views/backend/ib_schema/include/__delete.blade.php
+++ resources/views/backend/ib_schema/include/__delete.blade.php
@@ -1,18 +1,29 @@
-<div class="modal fade" id="deleteConfirmationModal" tabindex="-1" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteConfirmationModalLabel">{{ __('Are you sure?') }}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>{{ __('Please type "delete" to confirm.') }}</p>
-                    <input type="text" id="deleteConfirmationInput" class="form-control" placeholder="delete">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Cancel') }}</button>
-                    <button type="button" id="confirmDeleteButton" class="btn btn-danger">{{ __('Delete') }}</button>
+<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="deleteConfirmationModal" tabindex="-1" aria-labelledby="deleteConfirmationModal" aria-hidden="true">
+    <div class="modal-dialog top-1/2 !-translate-y-1/2 relative w-auto pointer-events-none">
+        <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding
+            rounded-md outline-none text-current">
+            <div class="p-6 space-y-4">
+                <div class="text-center">
+                    <iconify-icon icon="heroicons:question-mark-circle-16-solid" class="display-1 mb-3"></iconify-icon>
+                    <h3 class="mb-0">{{ __('Are you sure?') }}</h3>
+                    <p class="lead my-3">
+                        {{ __('Please type "delete" to confirm.') }}
+                    </p>
+                    <div class="input-area">
+                        <input type="text" id="deleteConfirmationInput" class="form-control" placeholder="delete">
+                    </div>
+                    <div class="action-btns mt-5">
+                        <button type="button" id="confirmDeleteButton" class="btn btn-dark inline-flex items-center justify-center">
+                            <iconify-icon class="text-xl ltr:mr-2 rtl:ml-2" icon="lucide:check"></iconify-icon>
+                            {{ __('Delete') }}
+                        </button>
+                        <button type="button" class="btn btn-danger inline-flex items-center justify-center mr-2" data-bs-dismiss="modal">
+                            <iconify-icon class="text-xl ltr:mr-2 rtl:ml-2" icon="lucide:x"></iconify-icon>
+                            {{ __('Cancel') }}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>

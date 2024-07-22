72a88d4efad05452d94f6aa2a4b75c06e1a9dcfe
naeemmunir <naeemali2020@gmail.com>
2024-07-22 22:08:41 +0500

    delete modal

--- resources/views/backend/ib_schema/include/__delete.blade.php
+++ resources/views/backend/ib_schema/include/__delete.blade.php
@@ -13,11 +13,11 @@
                         <input type="text" id="deleteConfirmationInput" class="form-control" placeholder="delete">
                     </div>
                     <div class="action-btns mt-5">
-                        <button type="button" id="confirmDeleteButton" class="btn btn-dark inline-flex items-center justify-center">
+                        <button type="button" id="confirmDeleteButton" class="btn btn-dark inline-flex items-center justify-center mr-2">
                             <iconify-icon class="text-xl ltr:mr-2 rtl:ml-2" icon="lucide:check"></iconify-icon>
                             {{ __('Delete') }}
                         </button>
-                        <button type="button" class="btn btn-danger inline-flex items-center justify-center mr-2" data-bs-dismiss="modal">
+                        <button type="button" class="btn btn-danger inline-flex items-center justify-center" data-bs-dismiss="modal">
                             <iconify-icon class="text-xl ltr:mr-2 rtl:ml-2" icon="lucide:x"></iconify-icon>
                             {{ __('Cancel') }}
                         </button>

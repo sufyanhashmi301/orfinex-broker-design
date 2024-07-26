9be76852fc623e129e6c183adb503afd5c16254d
Gulraiz Azam <gulraiz@mail.redsignal.net>
2024-07-26 15:05:37 +0500

    seeder issue resolved

--- database/seeders/KYCLevelSettingsSeeder.php
+++ database/seeders/KYCLevelSettingsSeeder.php
@@ -62,42 +62,20 @@ class KYCLevelSettingsSeeder extends Seeder
             );
         }
 
-        // Retrieve all KYC levels and their IDs
         $kycLevels = Kyclevel::pluck('id')->toArray();
-
-        // Retrieve all KYC forms that match the KYC level IDs
-        $kycForms = Kyc::whereIn('kyc_level_id', $kycLevels)->get();
-
-        foreach ($kycForms as $form) {
-            // Use updateOrCreate to add or update the KYC level setting for the current form
-            Kyclevelsetting::updateOrCreate(
-                [
-                    'kyc_level_id' => $form->kyc_level_id,
-                    'title' => $form->name,
-                ],
-                [
-                    'unique_code' => 'manual',
-                    'status' => true,
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]
-            );
-
-            // If the KYC level ID is 2 and the 'samsub' record does not already exist, add it
-            if ($form->kyc_level_id == 2) {
-                Kyclevelsetting::updateOrCreate(
-                    [
-                        'kyc_level_id' => $kycLevelIdLevel2,
-                        'title' => 'samsub',
-                    ],
-                    [
-                        'unique_code' => 'samsub',
-                        'status' => true,
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ]
-                );
-            }
-        }
+        Kyclevelsetting::updateOrCreate(
+            [
+                'kyc_level_id' => $kycLevelIdLevel2,
+                'title' => 'samsub',
+            ],
+            [
+                'unique_code' => 'samsub',
+                'status' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+            
+       
     }
 }

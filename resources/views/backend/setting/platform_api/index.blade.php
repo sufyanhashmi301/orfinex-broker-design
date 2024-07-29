8bb835116c5fafcd04bd32a5d9fa59952cb30588
naeemmunir <naeemali2020@gmail.com>
2024-07-29 13:15:03 +0500

    platform API pages design.

--- resources/views/backend/setting/platform_api/index.blade.php
+++ resources/views/backend/setting/platform_api/index.blade.php
@@ -20,6 +20,11 @@
                     {{ __('MetaTrader') }}
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="javascript:;" class="nav-link block font-medium font-Inter text-sm leading-tight capitalize rounded-md px-6 py-3 focus:outline-none focus:ring-0 dark:bg-slate-900 dark:text-slate-300">
+                    {{ __('DB Synchronization') }}
+                </a>
+            </li>
         </ul>
     </div>
     @yield('platform-content')

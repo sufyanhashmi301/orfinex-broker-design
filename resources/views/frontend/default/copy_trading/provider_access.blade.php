b0bab06615472409f75dab1497c8f2b128fb4385
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-08-12 14:57:35 +0500

    copytrading

--- resources/views/frontend/default/copy_trading/provider_access.blade.php
+++ resources/views/frontend/default/copy_trading/provider_access.blade.php
@@ -10,8 +10,8 @@
     </style>
 @endsection
 @section('content')
-    @if(setting('copy_trading_provider_access','platform_links',false))
-        <iframe src="{{setting('copy_trading_provider_access_show','platform_links','javascript:void(0);')}}"
+    @if(setting('copy_trading_follower_access_show','platform_links',false))
+        <iframe src="{{setting('copy_trading_follower_access','platform_links','javascript:void(0);')}}"
                 class="w-full h-screen"
                 frameborder="0"></iframe>
     @endif
--- routes/web.php
+++ routes/web.php
@@ -303,15 +303,15 @@ Route::get('user/economic_calendar', function () {
 
 Route::get('user/provider_access', function () {
 
-    return view('frontend.default.copy_trading.provider_access');
+    return view('frontend.prime_x.copy_trading.provider_access');
 })->name('user.provider_access')->middleware('secure_header');
 
 Route::get('user/follower_access', function () {
-    return view('frontend.default.copy_trading.follower_access');
+    return view('frontend.prime_x.copy_trading.follower_access');
 })->name('user.follower_access')->middleware('secure_header');
 
 Route::get('user/ratings', function () {
-    return view('frontend.default.copy_trading.ratings');
+    return view('frontend.prime_x.copy_trading.ratings');
 })->name('user.ratings')->middleware('secure_header');
 
 Route::post('/telegram/webhook', [TelegramController::class, 'webhook']);

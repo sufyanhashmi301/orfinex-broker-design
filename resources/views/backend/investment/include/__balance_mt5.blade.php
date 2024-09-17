b429db5e4efa114796240063073831830b39f817
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-09-17 18:30:10 +0500

    blance

--- resources/views/backend/investment/include/__balance_mt5.blade.php
+++ resources/views/backend/investment/include/__balance_mt5.blade.php
@@ -1,32 +1,5 @@
 @php
         $balance=0;
-        $cacheKey = 'mt5_db_connection_status';
-
-        // Check if the database is marked as unavailable
-        if (Cache::has($cacheKey) && Cache::get($cacheKey) === 'down') {
-            // Return 0 immediately without attempting to connect
-            return 0;
-        }
-
-        // Attempt to establish a database connection
-        try {
-            DB::connection('mt5_db')->getPdo();
-        } catch (\PDOException $e) {
-            \Log::error('MT5 DB connection failed: ' . $e->getMessage());
-            Cache::put($cacheKey, 'down', now()->addMinutes(5)); // Adjust the duration as needed
-            return 0;
-        }
-try {
-        $account = DB::connection('mt5_db')
-                    ->table('mt5_accounts')
-                    ->where('Login', $login)
-                    ->first();
-        if($account){
-            $balance = $account->Balance;
-        }
-        } catch (\Exception $e) {
-        $balance=0;
-}
 @endphp
 <strong
     class="green-color">{{ $balance.' '.$currency }}</strong>

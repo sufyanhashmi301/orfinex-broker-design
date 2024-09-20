b914fefac71f9ac27e32171301d0f8f916cabe93
naeemmunir <naeemali2020@gmail.com>
2024-09-20 19:51:26 +0500

    Mail Settings text change.

--- resources/views/backend/setting/email_setting/google-mail.blade.php
+++ resources/views/backend/setting/email_setting/google-mail.blade.php
@@ -11,7 +11,7 @@
                     <span class="text-2xl font-medium">{{ __('Gmail') }}</span>
                 </div>
                 <p class="card-text">
-                    {{ __('Set up the Google Mail server for email and send automated notifications to your customers about transactions, payments, and reminders.') }}
+                    {{ __('Configure your Google Mail (Gmail) server to send automated email notifications regarding transactions, payments, and reminders to your customers.') }}
                 </p>
             </div>
             <span class="badge bg-danger-500 text-danger-500 bg-opacity-30 capitalize">
@@ -22,32 +22,49 @@
             <div class="space-y-5">
                 <div>
                     <h6 class="text-lg font-medium mb-[6px] dark:text-white text-slate-900 mb-3">
-                        {{ __('Benefits:') }}
+                        {{ __('Key Benefits:') }}
                     </h6>
                     <ul>
-                        <li class="text-sm text-slate-600 dark:text-slate-300 py-1">
-                            {{ __('Notify customers instantly about transactions, payments and reminders via SMS.') }}
+                        <li class="text-sm text-slate-600 dark:text-slate-300 flex space-x-2 items-center py-1">
+                            <span class="h-[6px] w-[6px] bg-slate-900 dark:bg-slate-400 rounded-full inline-block"></span>
+                            <span>{{ __('Notify your customers instantly about their transaction statuses, payments, and important reminders via Gmail.') }}</span>
                         </li>
-                        <li class="text-sm text-slate-600 dark:text-slate-300 py-1">
-                            {{ __('Configure SMS notifications at customer and contact person level.') }}
+                        <li class="text-sm text-slate-600 dark:text-slate-300 flex space-x-2 items-center py-1">
+                            <span class="h-[6px] w-[6px] bg-slate-900 dark:bg-slate-400 rounded-full inline-block"></span>
+                            <span>{{ __('Enable seamless email notifications for both customers and their assigned contact persons, ensuring effective communication.') }}</span>
                         </li>
                     </ul>
                 </div>
+                <div class="py-[18px] px-6 font-normal font-Inter text-sm rounded-md bg-warning-500 bg-opacity-[14%] text-warning-500">
+                    <div class="flex items-center space-x-3 rtl:space-x-reverse mb-3">
+                        <iconify-icon class="text-2xl flex-0" icon="lucide:triangle-alert"></iconify-icon>
+                        <p class="flex-1 text-lg font-medium">
+                            {{ __('Important Guidelines:') }}
+                        </p>
+                    </div>
+                    <p class="text-sm text-warning-500">
+                        {{ __('Sending emails via Gmail is not recommended by us due to the security restrictions imposed by Google. These security measures may prevent the sending of emails from your CRM, even after making the necessary settings. Google may block email sending on certain servers, leading to unreliable delivery. While you can use Gmail if it works on your server, our support team will not assist in troubleshooting Gmail-related issues. For a more reliable solution, we recommend using a professional email provider such as SMTP servers (e.g., SendGrid, Mailgun) for better performance and support.') }}
+                    </p>
+                </div>
                 <div>
                     <h6 class="text-lg font-medium mb-[6px] dark:text-white text-slate-900">
-                        {{ __('Before you can connect Twilio with zojo invoice, you must') }}
+                        {{ __('Before Connecting Your Gmail Account:') }}
                     </h6>
+                    <p class="text-sm font-semibold dark:text-white mb-2">
+                        {{ __('To integrate Gmail with your CRM, follow these steps:') }}
+                    </p>
                     <ul>
-                        <li class="text-sm text-slate-600 dark:text-slate-300 py-1">
-                            {{ __('Create a Twilio account. ') }}
-                            <a href="" class="text-success-500">Sign up Now</a>
+                        <li class="text-sm text-slate-600 dark:text-slate-300 flex space-x-2 items-center py-1">
+                            <span class="h-[6px] w-[6px] bg-slate-900 dark:bg-slate-400 rounded-full inline-block"></span>
+                            <span>{{ __('Enable IMAP in Gmail Go to your Gmail settings and enable IMAP to allow email sending through third-party platforms.') }}</span>
                         </li>
-                        <li class="text-sm text-slate-600 dark:text-slate-300 py-1">
-                            {{ __('Go to console in Twilio and get your account SID and Auth Token.') }}
+                        <li class="text-sm text-slate-600 dark:text-slate-300 flex space-x-2 items-center py-1">
+                            <span class="h-[6px] w-[6px] bg-slate-900 dark:bg-slate-400 rounded-full inline-block"></span>
+                            <span>{{ __('Generate an App Password Due to Gmail’s enhanced security, you need to generate an app-specific password to connect Gmail with your CRM.') }}</span>
                         </li>
-                        <li class="text-sm text-slate-600 dark:text-slate-300 py-1">
-                            {{ __('Have an active phone number that works with Twilio. ') }}
-                            <a href="" class="text-success-500">Read More</a>
+                        <li class="text-sm text-slate-600 dark:text-slate-300 flex space-x-2 items-center py-1">
+                            <span class="h-[6px] w-[6px] bg-slate-900 dark:bg-slate-400 rounded-full inline-block"></span>
+                            <span>{{ __('Enter Credentials Enter your Gmail credentials and the generated app password into the CRM to initiate the connection.') }}</span>
                         </li>
                     </ul>
                 </div>

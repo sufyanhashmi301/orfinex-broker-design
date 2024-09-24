124c0668eaeda4f9f2a5a01411942672eec179a5
naeemmunir <naeemali2020@gmail.com>
2024-09-24 17:01:51 +0500

    copy trading menu.

--- resources/views/frontend/prime_x/copy_trading/index.blade.php
+++ resources/views/frontend/prime_x/copy_trading/index.blade.php
@@ -4,24 +4,47 @@
         .page-content {
             padding: 0 !important;
         }
+        .copy-trading__menu {
+            top: 22px;
+        }
+        .nav-tabs .nav-link.active {
+            color: #FED000 !important;
+            border-color: #FED000 !important;
+        }
+
+        @media (min-width: 960px) {
+            .copy-trading__menu {
+                left: 15px;
+            }
+        }
+        @media (min-width: 1200px) {
+            .copy-trading__menu {
+                left: 237px;
+            }
+        }
+        @media (min-width: 1600px) {
+            .copy-trading__menu {
+                left: 118px;
+            }
+        }
     </style>
 @endsection
 @section('content')
     <div class="relative">
-        <div class="absolute left-0">
-            <ul class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4">
+        <div class="absolute copy-trading__menu">
+            <ul class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0">
                 <li class="nav-item">
-                    <a href="{{ route('user.follower_access') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.follower_access') }}">
+                    <a href="{{ route('user.follower_access') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.follower_access') }}">
                         {{ __('Follower Access') }}
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('user.provider_access') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.provider_access') }}">
+                    <a href="{{ route('user.provider_access') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.provider_access') }}">
                         {{ __('Provider Access') }}
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('user.ratings') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.ratings') }}">
+                    <a href="{{ route('user.ratings') }}" class="nav-link w-full block font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 hover:border-transparent focus:border-transparent dark:text-slate-300 {{ isActive('user.ratings') }}">
                         {{ __('Ratings') }}
                     </a>
                 </li>

06cfaa7200af64711989159a434c27ea524ce3cb
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-09-26 01:24:03 +0500

    pending accounts

--- resources/views/frontend/prime_x/user/include/__demo_accounts.blade.php
+++ resources/views/frontend/prime_x/user/include/__demo_accounts.blade.php
@@ -1,43 +1,62 @@
-<div class="overflow-x-auto -mx-6">
-    <div class="inline-block min-w-full align-middle">
-        <div class="overflow-hidden ">
-            <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
-                <thead class="bg-slate-200 dark:bg-slate-700">
-                    <tr>
-                        <th scope="col" class="table-th">{{ __('Account Number') }}</th>
-                        <th scope="col" class="table-th">{{ __('Type') }}</th>
-                        <th scope="col" class="table-th">{{ __('Equity') }}</th>
-                        <th scope="col" class="table-th"></th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
-                    @foreach($demoForexAccounts as $account)
-                    <tr>
-                        <td class="table-td">
-                            <strong>{{$account->login}}</strong>
-                        </td>
-                        <td class="table-td">
-                            {{$account->trading_platform}} {{ucfirst($account->account_type)}}
-                        </td>
-                        <td class="table-td">
-                            {{$account->equity}} $
-                        </td>
-                        <td class="table-td">
-                            <div class="flex space-x-3 rtl:space-x-reverse">
-                                <a href="{{ route('user.kyc') }}" class="toolTip onTop action-btn" data-tippy-content="Deposit" data-tippy-theme="dark">
-                                    <iconify-icon icon="octicon:download-16"></iconify-icon>
-                                </a>
-                                <a href="javascript:;" class="toolTip onTop action-btn" data-tippy-content="Trade" data-tippy-theme="dark">
-                                    <iconify-icon icon="tabler:chart-candle"></iconify-icon>
-                                </a>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            {{  $demoForexAccounts->links() }}
-
+@if(!blank($pendingPlans = data_get($investments, 'pending', [])))
+    <div class="card mb-3">
+        <header class="card-header noborder">
+            <h4 class="card-title">{{ __('Pending Challenge (:count)',['count'=> count($pendingPlans) ]) }}</h4>
+        </header>
+        <div class="card-body p-6 pt-0">
+            <div class="overflow-x-auto -mx-6">
+                <div class="inline-block min-w-full align-middle">
+                    <div class="overflow-hidden ">
+                        <table class="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="table-th">{{ __('Title') }}</th>
+                                <th scope="col" class="table-th">{{ __('Fund') }}</th>
+                                <th scope="col" class="table-th">{{ __('Activation Date') }}</th>
+                                <th scope="col" class="table-th">{{ __('Daily Drawdown') }}</th>
+                                <th scope="col" class="table-th">{{ __('Max Drawdown') }}</th>
+                                <th scope="col" class="table-th">{{ __('Profit Target') }}</th>
+                                <th scope="col" class="table-th">{{ __('Detail') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($pendingPlans as $plan)
+                                <tr>
+                                    <td class="table-td">{{ data_get($plan->forexSchemaPhaseRule->forexSchemaPhase->forexSchema,'title') }}</td>
+                                    <td class="table-td">{{ data_get($plan,'amount_allotted')}}</td>
+                                    <td class="table-td">{{ data_get($plan,'term_start') ?? 'N/A'}}</td>
+                                    <td class="table-td">{{ data_get($plan,'daily_drawdown_limit') }}</td>
+                                    <td class="table-td">{{ data_get($plan,'max_drawdown_limit') }}</td>
+                                    <td class="table-td">{{ data_get($plan->forexSchemaPhaseRule,'profit_target')}}</td>
+                                    <td class="table-td">
+                                        <a href="{{route('user.invest.details',$plan->id)}}" class="inline-flex justify-center">
+                                        <span class="flex items-center">
+                                            <span>{{ __('Fund Matrics') }}</span>
+                                            <iconify-icon class="text-xl ltr:ml-2 rtl:mr-2"
+                                                          icon="lucide:chevron-right"></iconify-icon>
+                                        </span>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+@else
+    <div class="card mb-3">
+        <div class="card-body p-6">
+            <div class="flex items-center justify-center flex-col gap-3">
+                <img src="{{ asset('frontend/images/icon/danger.png') }}" alt="">
+                <p class="text-lg text-slate-600 dark:text-slate-100">
+                    {{ __("You don't have pending Challenge yet.") }}
+                </p>
+            </div>
+        </div>
+    </div>
+@endif
+
+

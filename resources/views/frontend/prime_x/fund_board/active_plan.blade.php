10ce5e684935e81dafc0f9e3dd39d2dc19382337
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-09-26 00:38:01 +0500

    active

--- resources/views/frontend/prime_x/fund_board/active_plan.blade.php
+++ resources/views/frontend/prime_x/fund_board/active_plan.blade.php
@@ -239,14 +239,14 @@
                     <li class="flex items-center justify-between text-sm text-slate-500 gap-2">
                         <span>{{ __('Today’s Permitted Loss:') }}</span>
                         <span class="text-slate-900 font-medium">
-                            {{ $invest->max_drawdown_limit }} {{ base_currency() }}
+                            {{ $invest->daily_drawdown_limit }} {{ base_currency() }}
                         </span>
                     </li>
                     <li class="flex items-center justify-between text-sm text-slate-500 gap-2">
                         <span>{{ __('Remaining Loss Limit:') }}</span>
                         <span class="text-slate-900 font-medium">
                             <?php $loss = number_format($todayScore['result']['net_Profit'], 2) ?? 0; ?>
-                            {{ $invest->max_drawdown_limit + $loss}} {{ base_currency() }}
+                            {{ $invest->daily_drawdown_limit + $loss}} {{ base_currency() }}
                         </span>
                     </li>
                 </ul>
@@ -271,13 +271,13 @@
                     <li class="flex items-center justify-between text-sm text-slate-500 gap-2">
                         <span>{{ __('Today’s Permitted Loss:') }}</span>
                         <span class="text-slate-900 font-medium">
-                            {{ $invest->daily_drawdown_limit}} {{base_currency()}}
+                            {{ $invest->max_drawdown_limit}} {{base_currency()}}
                         </span>
                     </li>
                     <li class="flex items-center justify-between text-sm text-slate-500 gap-2">
                         <span>{{ __('Remaining Loss Limit:') }}</span>
                         <span class="text-slate-900 font-medium">
-                            {{ $invest->daily_drawdown_limit + $todayScore['result']['total_Losses'] ?? 0 }} {{base_currency()}}
+                            {{ $invest->max_drawdown_limit + $todayScore['result']['total_Losses'] ?? 0 }} {{base_currency()}}
                         </span>
                     </li>
                 </ul>
@@ -620,49 +620,49 @@
         </div>
     </div>
 
-{{--    <div class="flex justify-between flex-wrap items-center mb-3">--}}
-{{--        <h4 class="font-medium text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-4 sm:mb-0 flex space-x-3 rtl:space-x-reverse">--}}
-{{--            {{ __('Overall Performance') }}--}}
-{{--        </h4>--}}
-{{--    </div>--}}
-{{--    <div class="card mb-6">--}}
-{{--        <div class="card-body p-6">--}}
-{{--            <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5">--}}
-{{--                <div class="card border border-slate-100 dark:border-slate-700 p-6">--}}
-{{--                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">--}}
-{{--                        {{ __('Balance') }}--}}
-{{--                    </div>--}}
-{{--                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">--}}
-{{--                        {{ $invest->max_balance}} {{base_currency()}}--}}
-{{--                    </h4>--}}
-{{--                </div>--}}
-{{--                <div class="card border border-slate-100 dark:border-slate-700 p-6">--}}
-{{--                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">--}}
-{{--                        {{ __('Profit') }}--}}
-{{--                    </div>--}}
-{{--                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">--}}
-{{--                        {{ $invest->profit}} {{base_currency()}}--}}
-{{--                    </h4>--}}
-{{--                </div>--}}
-{{--                <div class="card border border-slate-100 dark:border-slate-700 p-6">--}}
-{{--                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">--}}
-{{--                        {{ __('Growth') }}--}}
-{{--                    </div>--}}
-{{--                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">--}}
-{{--                        {{$growthPercentage}}%--}}
-{{--                    </h4>--}}
-{{--                </div>--}}
-{{--                <div class="card border border-slate-100 dark:border-slate-700 p-6">--}}
-{{--                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">--}}
-{{--                        {{ __('Days') }}--}}
-{{--                    </div>--}}
-{{--                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">--}}
-{{--                        {{\Carbon\Carbon::parse($invest->term_start)->diffInDays(\Carbon\Carbon::now())}}--}}
-{{--                    </h4>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    <div class="flex justify-between flex-wrap items-center mb-3">
+        <h4 class="font-medium text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-4 sm:mb-0 flex space-x-3 rtl:space-x-reverse">
+            {{ __('Overall Performance') }}
+        </h4>
+    </div>
+    <div class="card mb-6">
+        <div class="card-body p-6">
+            <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5">
+                <div class="card border border-slate-100 dark:border-slate-700 p-6">
+                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">
+                        {{ __('Balance') }}
+                    </div>
+                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">
+                        {{ $invest->max_balance}} {{base_currency()}}
+                    </h4>
+                </div>
+                <div class="card border border-slate-100 dark:border-slate-700 p-6">
+                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">
+                        {{ __('Profit') }}
+                    </div>
+                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">
+                        {{ $invest->profit}} {{base_currency()}}
+                    </h4>
+                </div>
+                <div class="card border border-slate-100 dark:border-slate-700 p-6">
+                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">
+                        {{ __('Growth') }}
+                    </div>
+                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">
+                        {{$growthPercentage}}%
+                    </h4>
+                </div>
+                <div class="card border border-slate-100 dark:border-slate-700 p-6">
+                    <div class="text-xs font-normal text-slate-600 dark:text-slate-400 mb-1">
+                        {{ __('Days') }}
+                    </div>
+                    <h4 class="text-base font-medium text-slate-600 whitespace-nowrap">
+                        {{\Carbon\Carbon::parse($invest->term_start)->diffInDays(\Carbon\Carbon::now())}}
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
 
 {{--    <div class="flex justify-between flex-wrap items-center mb-3">--}}
 {{--        <h4 class="font-medium text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-4 sm:mb-0 flex space-x-3 rtl:space-x-reverse">--}}

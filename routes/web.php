0b5edfad6d6bb0ece7d648e980ea3590d470f1a5
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-09-27 21:40:24 +0500

    match2pay payment methods

--- routes/web.php
+++ routes/web.php
@@ -219,6 +219,8 @@ Route::group(['controller' => StatusController::class, 'prefix' => 'status', 'as
 Route::group(['prefix' => 'ipn', 'as' => 'ipn.', 'controller' => IpnController::class], function () {
     Route::post('coinpayments', 'coinpaymentsIpn')->name('coinpayments');
     Route::post('nowpayments', 'nowpaymentsIpn')->name('nowpayments');
+    Route::post('bridgerpay', 'bridgerpayIpn')->name('bridgerpay');
+    Route::post('match2pay', 'match2payIpn')->name('match2pay');
     Route::post('cryptomus', 'cryptomusIpn')->name('cryptomus');
     Route::get('paypal', 'paypalIpn')->name('paypal');
     Route::post('mollie', 'mollieIpn')->name('mollie');

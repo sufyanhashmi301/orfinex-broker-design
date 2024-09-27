d87a67fb1cb99bf1ef027d4753a677a631643d09
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-09-27 21:49:52 +0500

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,6 +32,8 @@ class DatabaseSeeder extends Seeder
         $this->call(KycsTableSeeder::class);
         $this->call(UpdateGatewayLogoSeeder::class);
         $this->call(EmploymentSeeder::class);
+        $this->call(RiskBookSeeder::class);
+        $this->call(Match2PayGatewaySeeder::class);
 
 
     }

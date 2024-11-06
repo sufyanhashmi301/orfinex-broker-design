2f40a2c01c0e5956ed3e2af72eb907f191ca156a
irfan.rehman <irfan.rehman@x9hq.com>
2024-11-06 21:18:53 +0500

    email discalaimer seting

--- resources/views/backend/email/edit.blade.php
+++ resources/views/backend/email/edit.blade.php
@@ -156,9 +156,9 @@
                                             {{ __('Disclaimer') }}
                                         </label>
                                         <div class="form-switch ps-0">
-                                            <input type="hidden" value="0" name="disclaimer">
+                                            <input type="hidden" value="0" name="is_disclaimer">
                                             <label class="relative inline-flex h-6 w-[46px] items-center rounded-full transition-all duration-150 cursor-pointer">
-                                                <input type="checkbox" name="disclaimer" value="1" class="sr-only peer">
+                                                <input type="checkbox" name="is_disclaimer" value="1" class="sr-only peer" @checked($template->is_disclaimer)>
                                                 <span class="w-11 h-6 bg-gray-200 peer-focus:outline-none ring-0 rounded-full peer dark:bg-gray-900 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black-500"></span>
                                             </label>
                                         </div>
@@ -168,9 +168,9 @@
                                             {{ __('Risk Warning') }}
                                         </label>
                                         <div class="form-switch ps-0">
-                                            <input type="hidden" value="0" name="risk_warning">
+                                            <input type="hidden" value="0" name="is_risk_warning">
                                             <label class="relative inline-flex h-6 w-[46px] items-center rounded-full transition-all duration-150 cursor-pointer">
-                                                <input type="checkbox" name="risk_warning" value="1" class="sr-only peer">
+                                                <input type="checkbox" name="is_risk_warning" value="1" class="sr-only peer" @checked($template->is_risk_warning)>
                                                 <span class="w-11 h-6 bg-gray-200 peer-focus:outline-none ring-0 rounded-full peer dark:bg-gray-900 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black-500"></span>
                                             </label>
                                         </div>

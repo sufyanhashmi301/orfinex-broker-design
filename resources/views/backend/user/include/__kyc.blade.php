2ef164598373217d87995e15821927e2569a1037
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-11-22 12:50:14 +0500

    kyc update

--- resources/views/backend/user/include/__kyc.blade.php
+++ resources/views/backend/user/include/__kyc.blade.php
@@ -1,4 +1,4 @@
-@if($kyc==kyc_required_completed_level())
+@if($kyc >= kyc_required_completed_level())
     <span class="badge badge-success bg-opacity-30 text-success">{{ __('Verified') }}</span>
 @else
     <span class="badge badge-danger bg-opacity-30 text-danger">{{ __('Unverified') }}</span>

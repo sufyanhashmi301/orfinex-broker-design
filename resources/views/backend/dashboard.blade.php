8a9d6c58fad0f6fbf71d8f3e39a81535932998d9
naeemmunir <naeemali2020@gmail.com>
2024-11-30 16:03:39 +0500

    admin header design changes.

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="flex justify-between flex-wrap items-center mb-6">
         <h4 class="font-medium lg:text-2xl text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-4 sm:mb-0 flex space-x-3 rtl:space-x-reverse">
-            {{ setting('site_title', 'global') }} {{ __('Dashboard') }}
+            @yield('title')
         </h4>
     </div>
     @canany(['deposit-action','withdraw-action','kyc-action',])
--- resources/views/backend/include/__header.blade.php
+++ resources/views/backend/include/__header.blade.php
@@ -3,6 +3,9 @@
     <div class="app-header z-[999] ltr:ml-[248px] rtl:mr-[248px] dark:shadow-slate-700">
         <div class="flex justify-between items-center h-full">
             <div class="flex items-center md:space-x-4 space-x-2 xl:space-x-0 rtl:space-x-reverse vertical-box">
+                <p class="md:block hidden text-white font-light">
+                    {{ setting('site_title','common_settings') }}
+                </p>
                 <a href="{{route('home')}}" class="mobile-logo xl:hidden inline-block">
                     <img src="{{ asset(setting('site_logo','global')) }}" class="logo-unfold h-10" alt="Logo"/>
                     <img src="{{ asset(setting('site_logo','global')) }}" class="hidden logo-fold h-10" alt="Logo"/>
@@ -12,26 +15,129 @@
                 </button>
             </div>
 
-            <div class="nav-tools flex items-center lg:space-x-5 space-x-3 rtl:space-x-reverse leading-0">
-                <div class="relative">
-                    <select name="language" class="form-control !py-1 min-w-max" onchange="window.location.href=this.options[this.selectedIndex].value;">
-                        @foreach(\App\Models\Language::where('status',true)->get() as $lang)
-                            <option value="{{ route('language-update',['name'=> $lang->locale]) }}" @selected( app()->getLocale() == $lang->locale )>
-                                {{$lang->name}}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+            <div class="nav-tools flex items-center md:space-x-4 space-x-3 rtl:space-x-reverse leading-0">
                 <div>
-                    <a href="{{ route('admin.reportIssues') }}" class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] lg:bg-gray-500-f7 bg-slate-50 dark:bg-slate-900 lg:dark:bg-slate-900 dark:text-white text-slate-900 cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center">
-                        <iconify-icon class="text-slate-800 dark:text-white text-xl" icon="lucide:info"></iconify-icon>
-                    </a>
+                    <button class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] bg-slate-50 bg-opacity-10 text-white cursor-pointer rounded-lg text-[20px] flex flex-col items-center justify-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <iconify-icon class="text-white text-xl" icon="lucide:plus"></iconify-icon>
+                    </button>
+                    <div class="dropdown-menu z-10 hidden bg-white min-w-max dark:bg-slate-800 border dark:border-slate-700 !top-[10px] rounded-md overflow-hidden p-5">
+                        <div class="flex gap-6">
+                            <div class="space-y-5">
+                                <p class="flex items-center text-sm font-medium uppercase dark:text-white">
+                                    <iconify-icon class="text-sm mr-2" icon="lucide:layout-dashboard"></iconify-icon>
+                                    {{ __('General') }}
+                                </p>
+                                <ul class="space-y-3">
+                                    <li class="pb-2">
+                                        <a href="{{ route('admin.user.create') }}">
+                                            <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                <span class="leading-[1]">
+                                                    {{ __('Add New Customer') }}
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="space-y-5">
+                                <p class="flex items-center text-sm font-medium uppercase dark:text-white">
+                                    <iconify-icon class="text-sm mr-2" icon="lucide:layout-dashboard"></iconify-icon>
+                                    {{ __('Finance') }}
+                                </p>
+                                <ul class="space-y-3">
+                                    <li class="pb-2">
+                                        <a href="{{ route('admin.deposit.add') }}">
+                                            <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                <span class="leading-[1]">
+                                                    {{ __('Add Deposit') }}
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="pb-2">
+                                        <a href="{{ route('admin.withdraw.add') }}">
+                                            <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                <span class="leading-[1]">
+                                                    {{ __('Add Withdrawal') }}
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="space-y-5">
+                                <p class="flex items-center text-sm font-medium uppercase dark:text-white">
+                                    <iconify-icon class="text-sm mr-2" icon="lucide:layout-dashboard"></iconify-icon>
+                                    {{ __('Manage') }}
+                                </p>
+                                <ul class="space-y-3">
+                                    @canany(['role-list','role-create','role-edit','staff-list','staff-create','staff-edit'])
+                                        @canany(['staff-list','staff-create','staff-edit'])
+                                            <li class="pb-2">
+                                                <a href="{{route('admin.staff.index')}}">
+                                                    <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                        <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                        <span class="leading-[1]">
+                                                            {{ __('Add New Staff') }}
+                                                        </span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                        @endcanany
+                                    @endcanany
+
+                                    @can('schema-create')
+                                        <li class="pb-2">
+                                            <a href="{{ route('admin.accountType.create') }}">
+                                                <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                    <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                    <span class="leading-[1]">
+                                                        {{ __('Account Type') }}
+                                                    </span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    @endcan
+
+                                    @canany(['automatic-gateway-manage','manual-gateway-manage','deposit-list','deposit-action',
+                                            'withdraw-list','withdraw-method-manage','withdraw-action','target-manage','referral-create',
+                                            'referral-list','referral-edit','referral-delete','ranking-list','ranking-create','ranking-edit'])
+                                        @canany(['automatic-gateway-manage','manual-gateway-manage','deposit-list','deposit-action'])
+                                            <li class="pb-2">
+                                                <a href="{{ route('admin.deposit.method.list','auto') }}">
+                                                    <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                        <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                        <span class="leading-[1]">
+                                                            {{ __('Deposit Method') }}
+                                                        </span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li class="pb-2">
+                                                <a href="{{ route('admin.withdraw.method.list','auto') }}">
+                                                    <div class="flex space-x-2 items-start text-sm rtl:space-x-reverse">
+                                                        <iconify-icon icon="lucide:circle-plus" class="leading-[1]"> </iconify-icon>
+                                                        <span class="leading-[1]">
+                                                            {{ __('Withdrawal Method') }}
+                                                        </span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                        @endcanany
+                                    @endcanany
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- BEGIN: Toggle Theme -->
                 <div>
-                    <button id="themeMood" class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] lg:bg-gray-500-f7 bg-slate-50 dark:bg-slate-900 lg:dark:bg-slate-900 dark:text-white text-slate-900 cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center">
-                        <iconify-icon class="text-slate-800 dark:text-white text-xl dark:block hidden" id="moonIcon" icon="line-md:sunny-outline-to-moon-alt-loop-transition"></iconify-icon>
-                        <iconify-icon class="text-slate-800 dark:text-white text-xl dark:hidden block" id="sunIcon" icon="line-md:moon-filled-to-sunny-filled-loop-transition"></iconify-icon>
+                    <button id="themeMood" class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] hover:bg-slate-50 hover:bg-opacity-10 text-white cursor-pointer rounded-lg text-[20px] flex flex-col items-center justify-center">
+                        <iconify-icon class="text-white text-xl dark:block hidden" id="moonIcon" icon="line-md:sunny-outline-to-moon-alt-loop-transition"></iconify-icon>
+                        <iconify-icon class="text-white text-xl dark:hidden block" id="sunIcon" icon="line-md:moon-filled-to-sunny-filled-loop-transition"></iconify-icon>
                     </button>
                 </div>
                 <!-- END: TOggle Theme -->
@@ -44,23 +150,17 @@
                     @include('global.__notification_data',['notifications'=>$notifications,'totalUnread'=>$totalUnread,'totalCount'=>$totalCount])
                 </div>
                 <div>
-                    <a href="{{ route('admin.settings.index') }}" class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] lg:bg-gray-500-f7 bg-slate-50 dark:bg-slate-900 lg:dark:bg-slate-900 dark:text-white text-slate-900 cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center">
-                        <iconify-icon class="text-slate-800 dark:text-white text-xl" icon="lucide:settings"></iconify-icon>
+                    <a href="{{ route('admin.settings.index') }}" class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] hover:bg-slate-50 hover:bg-opacity-10 text-white cursor-pointer rounded-lg text-[20px] flex flex-col items-center justify-center">
+                        <iconify-icon class="text-white text-xl" icon="lucide:settings"></iconify-icon>
                     </a>
                 </div>
                 <div class="md:block hidden w-full">
-                    <button class="text-slate-800 dark:text-white focus:ring-0 focus:outline-none font-medium rounded-lg text-sm text-center inline-flex items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <div class="lg:h-8 lg:w-8 h-7 w-7 rounded-full flex-1 ltr:mr-[10px] rtl:ml-[10px]">
+                    <button class="text-slate-800 dark:text-white focus:ring-0 focus:outline-none font-medium text-sm text-center inline-flex items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="lg:h-8 lg:w-8 h-7 w-7 rounded-full">
                             <img src="{{ asset('frontend/images/all-img/user.png') }}" alt="user" class="block w-full h-full object-cover rounded-full">
                         </div>
-                        <span class="flex-none text-slate-600 dark:text-white text-sm font-normal items-center lg:flex hidden overflow-hidden text-ellipsis whitespace-nowrap">
-                            Admin
-                        </span>
-                        <svg class="w-[16px] h-[16px] dark:text-white hidden lg:inline-block text-base inline-block ml-[10px] rtl:mr-[10px]" aria-hidden="true" fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg>
                     </button>
-                    <div class="dropdown-menu z-10 hidden bg-white divide-y divide-slate-100 shadow min-w-max dark:bg-slate-800 border dark:border-slate-700 !top-[23px] rounded-md overflow-hidden">
+                    <div class="dropdown-menu z-10 hidden bg-white divide-y divide-slate-100 shadow min-w-max dark:bg-slate-800 border dark:border-slate-700 !top-[10px] rounded-md overflow-hidden">
                         <ul class="py-1 text-sm text-slate-800 dark:text-slate-200">
                             <li>
                                 <a href="{{ route('admin.profile') }}" class="block px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600 dark:hover:text-white font-inter text-sm text-slate-600 dark:text-white font-normal">
@@ -86,6 +186,11 @@
                             </li>
                         </ul>
                     </div>
+                </div>
+                <div>
+                    <button class="h-[28px] w-[28px] lg:h-[32px] lg:w-[32px] text-white cursor-pointer rounded-lg flex flex-col items-center justify-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <iconify-icon class="text-white text-2xl" icon="mdi:dots-grid"></iconify-icon>
+                    </button>
                 </div>
             </div>
         </div>

98320b4e3171fa9f1d053b4f0b0b10feada88fc2
naeemmunir <naeemali2020@gmail.com>
2024-12-03 17:20:15 +0500

    key feature limit, user profile and changelog design changes.

--- resources/views/frontend/prime_x/user/setting/include/__profile.blade.php
+++ resources/views/frontend/prime_x/user/setting/include/__profile.blade.php
@@ -1,55 +1,42 @@
-<div class="card mb-10">
-    <div class="card-body p-6">
-        <div class="flex items-center justify-between flex-wrap gap-5">
-            <div class="flex items-center">
-                <div class="flex-none">
-                    <div class="h-[100px] w-[100px] rounded-full border-[2.5px] border-primary ltr:mr-3 rtl:ml-3">
-                        <img src="@if(auth()->user()->avatar && file_exists('assets/'.auth()->user()->avatar)) {{asset($user->avatar)}} @else {{ asset('frontend/images/all-img/user.png') }}@endif" alt="" class="block w-full h-full object-cover rounded-full">
+<div class="innerMenu">
+    <div class="card overflow-hidden mb-5">
+        <div class="card-body py-1">
+            <div class="grid md:grid-cols-3 col-span-1 gap-px bg-slate-100 dark:bg-slate-700">
+                <div class="bg-white dark:bg-secondary p-4">
+                    <div class="text-center space-y-2">
+                        <p class="text-slate-800 dark:text-slate-300 text-sm mb-1 font-medium">
+                            {{ __('Balance') }}
+                        </p>
+                        <h6 class="text-slate-900 dark:text-white text-xl font-medium">
+                            ${{auth()->user()->totalForexBalance()}}
+                        </h6>
                     </div>
                 </div>
-                <div class="flex-1 text-start">
-                    <div class="text-left">
-                        <span class="text-xl text-slate-900 dark:text-white">{{auth()->user()->full_name}}</span><br>
-                        <span class="flex items-center text-slate-400 text-sm font-normal">
-                            {{ $user->rank->ranking }}
-                            <iconify-icon class="text-primary ml-1" icon="bxs:badge-check"></iconify-icon>
-                        </span>
+                <div class="bg-white dark:bg-secondary p-4">
+                    <div class="text-center space-y-2">
+                        <p class="text-slate-800 dark:text-slate-300 text-sm mb-1 font-medium">
+                            {{ __('Equity') }}
+                        </p>
+                        <h6 class="text-slate-900 dark:text-white text-xl font-medium">
+                            ${{auth()->user()->totalForexEquity()}}
+                        </h6>
                     </div>
                 </div>
-            </div>
-            <div class="flex flex-wrap sm:justify-end justify-center items-center gap-3">
-                <div class="md:w-auto w-full text-center bg-slate-50 dark:bg-body rounded py-3 px-6">
-                    <p class="text-xl text-slate-900 dark:text-white mb-2">
-                        ${{auth()->user()->totalForexBalance()}}
-                    </p>
-                    <p class="text-base text-slate-600">
-                        {{ __('Balance') }}
-                    </p>
-                </div>
-                <div class="md:w-auto w-full text-center bg-slate-50 dark:bg-body rounded py-3 px-6">
-                    <p class="text-xl text-slate-900 dark:text-white mb-2">
-                        ${{auth()->user()->totalForexEquity()}}
-                    </p>
-                    <p class="text-base text-slate-600">
-                        {{ __('Equity') }}
-                    </p>
-                </div>
-                <div class="md:w-auto w-full text-center bg-slate-50 dark:bg-body rounded py-3 px-6">
-                    <p class="text-xl text-slate-900 dark:text-white mb-2">
-                        0
-                    </p>
-                    <p class="text-base text-slate-600">
-                        {{ __('Wallet Balance') }}
-                    </p>
+                <div class="bg-white dark:bg-secondary p-4">
+                    <div class="text-center space-y-2">
+                        <p class="text-slate-800 dark:text-slate-300 text-sm mb-1 font-medium">
+                            {{ __('Wallet Balance') }}
+                        </p>
+                        <h6 class="text-slate-900 dark:text-white text-xl font-medium">
+                            {{ 0 }}
+                        </h6>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<h4 class="text-xl text-slate-900 mb-3">
-    {{ __('Personal Information') }}
-</h4>
 <div class="card">
     <div class="card-body p-6">
         <form action="{{ route('user.setting.profile-update') }}" id="profile-update-form" method="post" enctype="multipart/form-data">
@@ -60,7 +47,7 @@
                         <label for="exampleFormControlInput1" class="form-label">{{ __('First Name') }}</label>
                         <input
                             type="text"
-                            class="form-control !text-lg"
+                            class="form-control"
                             name="first_name"
                             value="{{ $user->first_name }}"
                             placeholder="{{ __('First Name') }}"
@@ -71,7 +58,7 @@
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Last Name') }}</label>
                         <input
                             type="text"
-                            class="form-control !text-lg"
+                            class="form-control"
                             name="last_name"
                             value="{{ $user->last_name }}"
                             placeholder="{{ __('Last Name') }}"
@@ -82,7 +69,7 @@
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Username') }}</label>
                         <input
                             type="text"
-                            class="form-control !text-lg"
+                            class="form-control"
                             name="username"
                             value="{{ $user->username }}"
                             placeholder="{{ __('Username') }}"
@@ -91,8 +78,8 @@
                     </div>
                     <div class="input-area relative">
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Gender') }}</label>
-                        <div class="input-group select2-lg">
-                            <select name="gender" id="kycTypeSelect" class="select2 form-control !text-lg w-full mt-2 py-2" required>
+                        <div class="input-group">
+                            <select name="gender" id="kycTypeSelect" class="select2 form-control w-full mt-2 py-2" required>
                                 @foreach(['male','female','other'] as $gender)
                                     <option @if($user->gender == $gender) selected @endif value="{{$gender}}">
                                         {{ ucfirst($gender) }}
@@ -107,7 +94,7 @@
                         <input
                             type="date"
                             name="date_of_birth"
-                            class="form-control !text-lg"
+                            class="form-control"
                             value="{{ $user->date_of_birth }}"
                             placeholder="{{ __('Date of Birth') }}"
                             @if($user->date_of_birth && !setting('customer_dob_edit', 'customer_permission')) disabled @endif
@@ -118,7 +105,7 @@
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Email Address') }}</label>
                         <input
                             type="email"
-                            class="form-control !text-lg"
+                            class="form-control"
                             value="{{ $user->email }}"
                             placeholder="{{ __('Email Address') }}"
                             @if($user->email && !setting('customer_email_edit', 'customer_permission')) disabled @endif
@@ -128,7 +115,7 @@
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Phone') }}</label>
                         <input
                             type="text"
-                            class="form-control !text-lg w-full"
+                            class="form-control w-full"
                             name="phone" id="phone"
                             value="{{ $user->phone }}"
                             placeholder="{{ __('Phone') }}"
@@ -141,7 +128,7 @@
                         </label>
                         <input
                             type="text"
-                            class="form-control !text-lg"
+                            class="form-control"
                             value="{{ $user->country }}"
                             placeholder="{{ __('Country') }}"
                             @if($user->country && !setting('customer_country_edit', 'customer_permission')) disabled @endif
@@ -150,23 +137,23 @@
 
                     <div class="input-area relative">
                         <label for="exampleFormControlInput1" class="form-label">{{ __('City') }}</label>
-                        <input type="text" class="form-control !text-lg" name="city" value="{{ $user->city }}" placeholder="{{ __('City') }}" />
+                        <input type="text" class="form-control" name="city" value="{{ $user->city }}" placeholder="{{ __('City') }}" />
                     </div>
                     <div class="input-area relative">
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Zip') }}</label>
-                        <input type="text" class="form-control !text-lg" name="zip_code" value="{{ $user->zip_code }}" placeholder="{{ __('Zip') }}" />
+                        <input type="text" class="form-control" name="zip_code" value="{{ $user->zip_code }}" placeholder="{{ __('Zip') }}" />
                     </div>
                     <div class="input-area relative">
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Address') }}</label>
-                        <input type="text" class="form-control !text-lg" name="address" value="{{ $user->address }}" placeholder="{{ __('Address') }}" />
+                        <input type="text" class="form-control" name="address" value="{{ $user->address }}" placeholder="{{ __('Address') }}" />
                     </div>
                     <div class="input-area relative">
                         <label for="exampleFormControlInput1" class="form-label">{{ __('Joining Date') }}</label>
-                        <input type="text" class="form-control !text-lg disabled" value="{{ carbonInstance($user->created_at)->toDayDateTimeString() }}" placeholder="{{ __('Joining Date') }}" disabled />
+                        <input type="text" class="form-control disabled" value="{{ carbonInstance($user->created_at)->toDayDateTimeString() }}" placeholder="{{ __('Joining Date') }}" disabled />
                     </div>
                 </div>
 
-                <div class="text-right mt-4">
+                <div class="text-right mt-10">
                     <button type="submit" class="btn btn-primary inline-flex items-center justify-center" id="profile-update-save">{{ __('Save Changes') }}</button>
                 </div>
             </div>

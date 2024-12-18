690d92cc1ac7038533722941b11f9c6506ca4a36
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2024-12-19 03:52:17 +0500

    referral bonus

--- app/Listeners/RewardUser.php
+++ app/Listeners/RewardUser.php
@@ -52,15 +52,15 @@ class RewardUser
             //send email to parent/referral user
             $this->mailNotify($referral->email, 'new_user_ib', $shortcodes);
             // Sign Up Referral Bonus
-            if (setting('sign_up_referral', 'permission') && null !== $event->user->email_verified_at) {
-
-                $referralBonus = (float) setting('referral_bonus', 'fee');
-                // User who was sharing link
-                $provider = $referral->user;
-                $provider->increment('profit_balance', $referralBonus);
-                Txn::new($referralBonus, 0, $referralBonus, 'system', 'Referral Bonus via '.$event->user->full_name, TxnType::Referral, TxnStatus::Success, null, null, $provider->id);
-
-            }
+//            if (setting('sign_up_referral', 'permission') && null !== $event->user->email_verified_at) {
+//
+//                $referralBonus = (float) setting('referral_bonus', 'fee');
+//                // User who was sharing link
+//                $provider = $referral->user;
+////                $provider->increment('profit_balance', $referralBonus);
+//                Txn::new($referralBonus, 0, $referralBonus, 'system', 'Referral Bonus via '.$event->user->full_name, TxnType::Referral, TxnStatus::Success, null, null, $provider->id);
+//
+//            }
 
         }
     }

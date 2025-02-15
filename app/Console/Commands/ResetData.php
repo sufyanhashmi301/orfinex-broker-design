d7d50e9c85883cd965a26ace063dda7c057b4861
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2025-02-16 00:15:32 +0500

    reset data

--- app/Console/Commands/ResetData.php
+++ app/Console/Commands/ResetData.php
@@ -136,7 +136,7 @@ class ResetData extends Command
         Artisan::call('db:seed');
 
         $rank = Ranking::find(1);
-        $sitename = 'mymaamarkets';
+        $sitename = 'bluvoxmarkets';
 
         $dataUser = [
             'ranking_id' => $rank->id,
@@ -159,7 +159,7 @@ class ResetData extends Command
             'first_name' => 'Super',
             'last_name' => 'Admin',
             'email' => 'admin@'.$sitename.'.com',
-            'password' => Hash::make('Dubai@MyMAA@2024'),
+            'password' => Hash::make('bluvoxmarkets@2025'),
             'role' => 1,
         ]);
         $data = [

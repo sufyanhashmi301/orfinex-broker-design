988a7dfb279f30f143f1bbcd97ed7d8f70959860
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2025-02-21 22:02:11 +0500

    level seed er

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -17,9 +17,9 @@ class LevelSeeder extends Seeder
     {
         DB::table('levels')->insert([
             ['title' => 'Level 1', 'level_order' => 1],
-//            ['title' => 'Level 2', 'level_order' => 2],
-//            ['title' => 'Level 3', 'level_order' => 3],
-//            ['title' => 'Level 4', 'level_order' => 4],
-//            ['title' => 'Level 5', 'level_order' => 5],
+            ['title' => 'Level 2', 'level_order' => 2],
+            ['title' => 'Level 3', 'level_order' => 3],
+            ['title' => 'Level 4', 'level_order' => 4],
+            ['title' => 'Level 5', 'level_order' => 5],
         ]);    }
 }

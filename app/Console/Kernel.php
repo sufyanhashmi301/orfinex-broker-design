8abe0950d6a0c78a77022082c04eaaf244750a32
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2025-02-24 10:02:21 +0500

    kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('exchange:update-rates')->everyThirtyMinutes();
         $schedule->command('tokens:update-rates')->everyThirtyMinutes();
         $schedule->command('rebate:distribution')->everyMinute();
-        $schedule->command('sync:forex-accounts-via-email-banex')->everyFiveMinutes();
+        $schedule->command('sync:forex-accounts-via-email-banex')->everyMinute();
 //        if(url('/') == 'http://brokerdemo.brokeret.com') {
 //            $schedule->command('reset:data')->daily();
 //        }

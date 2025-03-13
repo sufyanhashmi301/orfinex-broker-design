b9b4681ea1b5c9b547ea444631013590f97e83ed
irfan.rehman <irfan.rehman@x9hq.com>
2025-03-13 18:13:20 +0500

    here account type added in user side accounts and account mapping files name changed and account schema model changes

---
+++ resources/views/backend/investment/modal/__change_schema.blade.php
@@ -0,0 +1,29 @@
+<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="changeForexSchema" tabindex="-1" aria-labelledby="changeForexSchema" aria-hidden="true">
+    <div class="modal-dialog top-1/2 !-translate-y-1/2 relative w-auto pointer-events-none">
+        <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white dark:bg-dark bg-clip-padding rounded-md outline-none text-current">
+            <div class="relative bg-white rounded-lg shadow dark:bg-dark">
+                <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600">
+                    <div>
+                        <h3 class="text-xl font-medium dark:text-white capitalize" id="modalTitle">
+                            {{ __('Change Account Schema') }}
+                        </h3>
+                        <p class="dark:text-white">
+                            {{ __('Account:') }}
+                            <span class="fw-bold" id="update-forex-schema-modal-login"></span>
+                        </p>
+                    </div>
+                    <button type="button" class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white" data-bs-dismiss="modal">
+                        <svg aria-hidden="true" class="w-5 h-5 fill-black dark:fill-white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="sr-only">{{ __('Close modal') }}</span>
+                    </button>
+                </div>
+                <div class="p-6 space-y-4">
+                    <!-- The AJAX loaded form or content will appear here -->
+                    <div id="view_forex_schema_form"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

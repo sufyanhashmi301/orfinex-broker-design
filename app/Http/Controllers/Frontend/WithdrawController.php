3935fffd1c41f42853bcb850b494ff44b8266f65
sufyanhashmi301 <bilawal.developer@gmail.com>
2025-04-09 15:09:56 +0500

    withdraw

--- app/Http/Controllers/Frontend/WithdrawController.php
+++ app/Http/Controllers/Frontend/WithdrawController.php
@@ -253,8 +253,8 @@ class WithdrawController extends Controller
      */
     public function withdrawNow(Request $request)
     {
-        try {
-            DB::beginTransaction();
+//        try {
+//            DB::beginTransaction();
 
             $validationResult = $this->validateWithdrawal($request);
             if (!$validationResult) {
@@ -293,11 +293,11 @@ class WithdrawController extends Controller
                 // Create the transaction before attempting the withdrawal (even if the withdrawal fails later)
                 return $this->processWithdrawal($validationResult);
             }
-        } catch (Exception $e) {
-            DB::rollBack();
-            notify()->error(__('An error occurred while processing your withdrawal. Please try again later.'), 'Error');
-            return redirect()->back()->withInput();
-        }
+//        } catch (Exception $e) {
+//            DB::rollBack();
+//            notify()->error(__('An error occurred while processing your withdrawal. Please try again later.'), 'Error');
+//            return redirect()->back()->withInput();
+//        }
     }
 
     public function resendOtp(Request $request)

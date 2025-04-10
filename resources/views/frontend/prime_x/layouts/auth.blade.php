06e5e10d685ab3b75242b9d6219725f468b283d4
naeemmunir <naeemali2020@gmail.com>
2025-04-10 13:41:51 +0500

    user side auth pages design changes.

--- resources/views/frontend/prime_x/layouts/auth.blade.php
+++ resources/views/frontend/prime_x/layouts/auth.blade.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
 <html lang="en" class="light">
     @include('frontend::include.__head')
-    <body class="font-inter dashcode-app" id="body_class">
+    <body class=" font-inter skin-default">
         <x:notify-messages/>
-
-        @yield('content')
+        <div class="loginwrapper">
+            <div class="lg-inner-column">
+                <div class="left-column relative">
+                    <div class="w-full h-full flex items-center justify-around bg-cover bg-no-repeat bg-center" style="background-image:url('https://cdn.brokeret.com/crm-assets/login-image/c19.png')">
+                        <div class="mx-auto max-w-xs text-center">
+                            <a href="{{ route('home')}}" class="">
+                                <img src="{{ asset(setting('site_logo','global')) }}" class="h-[56px]" alt="{{ __('Logo') }}">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-column relative">
+                    <div class="inner-content h-full flex flex-col bg-white dark:bg-slate-800">
+                        <div class="h-full flex flex-col justify-center">
+                            <div class="w-full max-w-lg mx-auto">
+                                <div class="mobile-logo text-center mb-6 lg:hidden block">
+                                    <a href="{{ route('home')}}">
+                                        <img src="{{ asset(setting('site_logo','global')) }}" alt="{{ __('Logo') }}" class="h-[56px]">
+                                    </a>
+                                </div>
+                                @yield('content')
+                            </div>
+                        </div>
+                        @if(setting('copyright_text', 'common_settings'))
+                            <div class="auth-footer text-center mt-5">
+                                {{ setting('copyright_text', 'common_settings') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
 
         @include('frontend::include.__script')
 

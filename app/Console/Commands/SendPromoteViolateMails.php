1c8e879f328a35cf4208e78bf0e554fcd31f71dc
Fahad <fahad.nasir771@gmail.com>
2025-04-11 16:01:39 +0500

    Automated Email Issue

--- app/Console/Commands/SendPromoteViolateMails.php
+++ app/Console/Commands/SendPromoteViolateMails.php
@@ -42,7 +42,7 @@ class SendPromoteViolateMails extends Command
 
             $account_phase = $account->getPhaseSnapshotData();
 
-            if(!$account_phase) {
+            if(!$account_phase || !$account || $account->user) {
                 continue;
             }
 

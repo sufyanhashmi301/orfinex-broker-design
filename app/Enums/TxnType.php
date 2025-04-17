13550d5cfc173ebd6a8d614f57944ad4ca9e4eee
naeemmunir <naeemali2020@gmail.com>
2025-04-17 11:57:59 +0500

    recent transactions design changes.

--- app/Enums/TxnType.php
+++ app/Enums/TxnType.php
@@ -4,53 +4,53 @@ namespace App\Enums;
 
 enum TxnType: string
 {
-case Deposit = 'deposit';
-case DemoDeposit = 'demo_deposit';
-case Subtract = 'subtract';
-case ManualDeposit = 'manual_deposit';
-case SendMoney = 'send_money';
-case ReceiveMoney = 'receive_money';
-case SendMoneyInternal = 'send_money_internal';
-case ReceiveMoneyInternal = 'receive_money_internal';
-case Exchange = 'exchange';
-case Referral = 'referral';
-case SignupBonus = 'signup_bonus';
-case Bonus = 'bonus';
-case BonusSubtract = 'bonus_subtract';
-case BonusRefund = 'bonus_refund';
-case Withdraw = 'withdraw';
-case WithdrawAuto = 'withdraw_auto';
-case Interest = 'interest';
-case Refund = 'refund';
-case IbBonus = 'ib_bonus';
-case MultiIB = 'multi_ib';
-case IB = 'ib';
+    case Deposit = 'deposit';
+    case DemoDeposit = 'demo_deposit';
+    case Subtract = 'subtract';
+    case ManualDeposit = 'manual_deposit';
+    case SendMoney = 'send_money';
+    case ReceiveMoney = 'receive_money';
+    case SendMoneyInternal = 'send_money_internal';
+    case ReceiveMoneyInternal = 'receive_money_internal';
+    case Exchange = 'exchange';
+    case Referral = 'referral';
+    case SignupBonus = 'signup_bonus';
+    case Bonus = 'bonus';
+    case BonusSubtract = 'bonus_subtract';
+    case BonusRefund = 'bonus_refund';
+    case Withdraw = 'withdraw';
+    case WithdrawAuto = 'withdraw_auto';
+    case Interest = 'interest';
+    case Refund = 'refund';
+    case IbBonus = 'ib_bonus';
+    case MultiIB = 'multi_ib';
+    case IB = 'ib';
 
     public function label(): string
-{
-    return match($this) {
-        self::Deposit => 'Deposit',
-        self::DemoDeposit => 'Demo Deposit',
-        self::Subtract => 'Subtract',
-        self::ManualDeposit => 'Manual Deposit',
-        self::SendMoney => 'External Send Money',
-        self::ReceiveMoney => 'External Receive Money',
-        self::SendMoneyInternal => 'Internal Send Money',
-        self::ReceiveMoneyInternal => 'Internal Receive Money',
-        self::Exchange => 'Exchange',
-        self::Referral => 'Referral',
-        self::SignupBonus => 'Signup Bonus',
-        self::Bonus => 'Bonus',
-        self::BonusSubtract => 'Bonus Subtract',
-        self::BonusRefund => 'Bonus Refund',
-        self::Withdraw => 'Withdraw',
-        self::WithdrawAuto => 'Withdraw Auto',
-        self::Interest => 'Interest',
-        self::Refund => 'Refund',
-        self::MultiIB => 'Multi IB',
-        self::IB => 'IB',
-        self::IbBonus => 'IB Bonus',
-    };
-}
+    {
+        return match($this) {
+            self::Deposit => 'Deposit',
+            self::DemoDeposit => 'Demo Deposit',
+            self::Subtract => 'Subtract',
+            self::ManualDeposit => 'Manual Deposit',
+            self::SendMoney => 'External Send Money',
+            self::ReceiveMoney => 'External Receive Money',
+            self::SendMoneyInternal => 'Internal Send Money',
+            self::ReceiveMoneyInternal => 'Internal Receive Money',
+            self::Exchange => 'Exchange',
+            self::Referral => 'Referral',
+            self::SignupBonus => 'Signup Bonus',
+            self::Bonus => 'Bonus',
+            self::BonusSubtract => 'Bonus Subtract',
+            self::BonusRefund => 'Bonus Refund',
+            self::Withdraw => 'Withdraw',
+            self::WithdrawAuto => 'Withdraw Auto',
+            self::Interest => 'Interest',
+            self::Refund => 'Refund',
+            self::MultiIB => 'Multi IB',
+            self::IB => 'IB',
+            self::IbBonus => 'IB Bonus',
+        };
+    }
 }
 

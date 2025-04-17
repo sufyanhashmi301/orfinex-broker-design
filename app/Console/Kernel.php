13cdd63ad2b43601d7c02a2955f0c5eb7e8c1e18
sufyanhashmi301 <sufyanhashmi301@gmail.com>
2025-04-17 18:08:50 +0500

    kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('rebate:distribution')->everyFiveMinutes();
+        $schedule->command('rebate:distribution')->everyTenMinutes();
         $schedule->command('exchange:update-rates')->everyThirtyMinutes();
         $schedule->command('tokens:update-rates')->everyThirtyMinutes();
 //        $schedule->command('sync:forex-accounts-via-email')->everyFiveMinutes();

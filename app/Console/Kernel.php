2c8b7620dea7b332cd03db34ab0eacd34605cca8
sufyanhashmi301 <bilawal.developer@gmail.com>
2025-05-08 17:06:13 +0500

    kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('rebate:distribution')->everyTenMinutes();
+//        $schedule->command('rebate:distribution')->everyTenMinutes();
         $schedule->command('users:delete-stale')->daily();
         $schedule->command('exchange:update-rates')->everyThirtyMinutes();
         $schedule->command('tokens:update-rates')->everyThirtyMinutes();

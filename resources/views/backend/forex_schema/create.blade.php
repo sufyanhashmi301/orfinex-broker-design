6846682afbbc89a598bb062d9fa8814d28f0b18d
naeemmunir <naeemali2020@gmail.com>
2025-05-27 15:17:05 +0500

    is global radio input

--- resources/views/backend/forex_schema/create.blade.php
+++ resources/views/backend/forex_schema/create.blade.php
@@ -64,7 +64,7 @@
                         <div id="global_account" class="input-area flex items-center hidden">
                             <input class="form-check-input" type="hidden" value="0" name="is_global">
                             <label class="relative inline-flex h-6 w-[46px] items-center rounded-full transition-all duration-150 cursor-pointer">
-                                <input type="checkbox" name="is_global" value="1" class="sr-only peer cursor-not-allowed" disabled checked>
+                                <input type="radio" name="is_global" value="1" class="sr-only peer">
                                 <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none ring-0 rounded-full peer dark:bg-gray-900 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black-500"></div>
                             </label>
                             <div class="flex flex-col ml-5">
@@ -81,7 +81,7 @@
                                 <label class="form-label" for="">
                                     {{ __('Select rebate rules') }}
                                     <span class="text-xs font-Inter font-normal text-slate-600 block">
-                                        {{ __('Choose countries to display this forex scheme. Select "All" to show it globally.') }}
+                                        {{ __('Visible to users based on IB Group rebate rule configuration.') }}
                                     </span>
                                 </label>
                                 <select name="rebate_rules[]" id="rebateRuleSelect" class="select2 form-control w-full h-9" placeholder="Manage Country" multiple>

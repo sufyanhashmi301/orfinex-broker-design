e893e148d00f2393bb80538c6c8063e6269ff851
sufyanhashmi <sufyanhashmi301@gmail.com>
2025-06-13 17:15:39 +0500

    remove grace period command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('rebate:distribution')->everyTenMinutes()->withoutOverlapping();
-        $schedule->command('users:delete-stale')->daily();
+        // $schedule->command('users:delete-stale')->daily();
         $schedule->command('exchange:update-rates')->everyThirtyMinutes();
         $schedule->command('tokens:update-rates')->everyThirtyMinutes();
 //        $schedule->command('sync:forex-accounts-via-email')->everyFiveMinutes();

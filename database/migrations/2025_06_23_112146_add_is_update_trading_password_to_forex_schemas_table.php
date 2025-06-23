b18c15f45e970ee59042e5108893f652816e8968
irfan.rehman <irfan.rehman@x9hq.com>
2025-06-23 18:01:06 +0500

    restriction on change trading password

---
+++ database/migrations/2025_06_23_112146_add_is_update_trading_password_to_forex_schemas_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('forex_schemas', function (Blueprint $table) {
+            $table->boolean('is_update_trading_password')->default(false)->after('is_cent_account');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('forex_schemas', function (Blueprint $table) {
+            $table->dropColumn('is_update_trading_password');
+        });
+    }
+};

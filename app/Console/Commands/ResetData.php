9c41aedcb7f0e3eb95a61cdc4b35f4629cb33361
sufyanhashmi <sufyanhashmi301@gmail.com>
2025-07-18 17:37:26 +0500

    reset data

--- app/Console/Commands/ResetData.php
+++ app/Console/Commands/ResetData.php
@@ -140,7 +140,7 @@ class ResetData extends Command
         Artisan::call('db:seed');
 
         $rank = Ranking::find(1);
-        $sitename = 'brokeret';
+        $sitename = 'knightmarkets';
 
         $dataUser = [
             'ranking_id' => $rank->id,

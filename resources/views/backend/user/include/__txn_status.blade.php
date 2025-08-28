6be1565a2d219883a37e025587c88ab0e60ffedd
sufyanhashmi <sufyanhashmi301@gmail.com>
2025-08-28 22:46:11 +0500

    transaction status

--- resources/views/backend/user/include/__txn_status.blade.php
+++ resources/views/backend/user/include/__txn_status.blade.php
@@ -3,15 +3,35 @@
         <div class="badge badge-warning capitalize">
             {{ __('Pending') }}
         </div>
-        @break
+    @break
+
     @case('success')
         <div class="badge badge-success capitalize">
             {{ __('Success') }}
         </div>
-        @break
+    @break
+
     @case('failed')
         <div class="badge badge-danger capitalize">
             {{ __('Failed') }}
         </div>
-        @break
+    @break
+
+    @case('review')
+        <div class="badge badge-info capitalize">
+            {{ __('Review') }}
+        </div>
+    @break
+
+    @case('none')
+        <div class="badge badge-secondary capitalize">
+            {{ __('None') }}
+        </div>
+    @break
+
+    @default
+        <div class="badge badge-secondary capitalize">
+            {{ __(ucfirst($status)) }}
+        </div>
+    @break
 @endswitch

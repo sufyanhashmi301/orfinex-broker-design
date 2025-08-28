0cdf157d3075639452a6ff2995e7be942bb77c4c
naeemmunir <naeemali2020@gmail.com>
2025-08-28 15:39:01 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,26 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // Frontend entrypoints (for @vite)
                 'assets/common/css/style.css',
                 'assets/common/js/index.js',
             ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // frontend build
+        manifest: true,
+        rollupOptions: {
+            input: [
+                'assets/common/css/style.css',
+                'assets/common/js/index.js',
+            ],
+        },
+    },
     resolve: {
         alias: {
-            '@brokeret': '/assets/common',
-        }
+            '@brokeret': '/assets/common', // optional alias
+        },
     },
 });

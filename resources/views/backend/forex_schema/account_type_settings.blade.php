bc4b8a6a79f3e14170a539b1c6f6ac56eac8b006
irfan.rehman <irfan.rehman@x9hq.com>
2025-09-15 13:17:19 +0500

    account type setting descripition setting

--- resources/views/backend/forex_schema/account_type_settings.blade.php
+++ resources/views/backend/forex_schema/account_type_settings.blade.php
@@ -16,45 +16,40 @@
                 @include('backend.setting.site_setting.include.__global')
             @endif
         @endforeach
-    </div>
-    <div class="card mt-5">
-        <div class="card-body p-6">
-            <h4 class="font-medium text-base mb-3">{{ __('Account Type Settings') }}</h4>
-            <p class="text-sm text-slate-700 dark:text-slate-200 mb-3">
-                {{ __('We have 3 account categories:') }}
-                <span class="font-semibold">{{ __('Global') }}</span>,
-                <span class="font-semibold">{{ __('Country & Tags') }}</span>,
-                {{ __('and') }}
-                <span class="font-semibold">{{ __('IB Rebate Rules') }}</span>.
-            </p>
+        <div class="card">
+            <div class="card-body p-6 space-y-4">
+                <h4 class="card-title">{{ __('Account Type Settings') }}</h4>
+                <p class="card-text">{{ __('We have 3 account categories:') }} <span class="font-semibold">{{ __('Global, Country & Tags, and IB Rebate Rules.') }}</span></p>
 
-            <ol class="list-decimal ltr:ml-5 rtl:mr-5 space-y-3 text-sm text-slate-700 dark:text-slate-200">
-                <li>
-                    <span class="font-semibold">{{ __('Show Global Accounts with Country & Tags') }}</span>
-                    <ul class="list-disc ltr:ml-5 rtl:mr-5 mt-1 space-y-1">
-                        <li><span class="font-semibold">{{ __('Enabled') }}</span>: {{ __('Global accounts will also appear for users that match by country or tags.') }}</li>
-                        <li><span class="font-semibold">{{ __('Disabled') }}</span>: {{ __('Global accounts will be hidden; only country/tag-specific accounts will show.') }}</li>
-                    </ul>
-                </li>
-                <li>
-                    <span class="font-semibold">{{ __('Show Global Accounts with IB Rebate Rules') }}</span>
-                    <ul class="list-disc ltr:ml-5 rtl:mr-5 mt-1 space-y-1">
-                        <li><span class="font-semibold">{{ __('Enabled') }}</span>: {{ __('Global accounts will also appear for users within the IB network.') }}</li>
-                        <li><span class="font-semibold">{{ __('Disabled') }}</span>: {{ __('Global accounts will be hidden; users will only see accounts tied to their IB group.') }}</li>
-                    </ul>
-                </li>
-            </ol>
+                <ol class="list-decimal ml-4 pl-5 space-y-3">
+                    <li>
+                        <h5 class="text-sm font-semibold">{{ __('Show Global Accounts with Country & Tags') }}</h5>
+                        <ul class="list-disc ml-4 pl-5 space-y-1">
+                            <li><span class="font-semibold">{{ __('Enabled:') }}</span> {{ __('Global accounts will also appear for users that match by country or tags.') }}</li>
+                            <li><span class="font-semibold">{{ __('Disabled:') }}</span> {{ __('Global accounts will be hidden; only country/tag-specific accounts will show.') }}</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h5 class="text-sm font-semibold">{{ __('Show Global Accounts with IB Rebate Rules') }}</h5>
+                        <ul class="list-disc ml-4 pl-5 space-y-1">
+                            <li><span class="font-semibold">{{ __('Enabled:') }}</span> {{ __('Global accounts will also appear for users within the IB network.') }}</li>
+                            <li><span class="font-semibold">{{ __('Disabled:') }}</span> {{ __('Global accounts will be hidden; users will only see accounts tied to their IB group.') }}</li>
+                        </ul>
+                    </li>
+                </ol>
 
-            <hr class="my-4">
-<br>
-            <h4 class="font-medium text-base mb-3">{{ __('Exception') }}</h4>
-            <p class="text-sm text-slate-700 dark:text-slate-200 mb-1">{!! __('When :strong, there is a toggle:', ['strong' => '<span class="font-semibold">'.__('adding/updating an account type').'</span>']) !!}</p>
-            <p class="text-sm font-semibold mb-1">{{ __('“Set as Global Account”') }}</p>
-            <ul class="list-disc ltr:ml-5 rtl:mr-5 space-y-1 text-sm text-slate-700 dark:text-slate-200">
-                <li>{{ __('If this is enabled, it overrides country, tag, and IB restrictions.') }}</li>
-                <li>{!! __('The account will be visible to :strong, regardless of the above settings.', ['strong' => '<span class="font-semibold">'.__('all users').'</span>']) !!}</li>
-            </ul>
+                <div class="border-t pt-3 mt-2 space-y-2">
+                    <h4 class="card-title">{{ __('Exception') }}</h4>
+                    <p>{{ __('When adding/updating an account type, there is a toggle:') }}</p>
+                    <p class="italic font-semibold">{{ __('“Set as Global Account”') }}</p>
+                    <ul class="list-disc ml-4 pl-5 space-y-1">
+                        <li>{{ __('If this is enabled, it overrides country, tag, and IB restrictions.') }}</li>
+                        <li>{{ __('The account will be visible to all users, regardless of the above settings.') }}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
+    
 @endsection
 
